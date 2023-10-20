a07aa2794126e156ed27ebc762cffe84cfc8d19d
meilakh <meilakh@google.com>
2023-10-20 09:04:29 +0000

    feat: add get_dataset tests

---
+++ google/cloud/odbc/integration_tests/apis/get_dataset_test.cc
@@ -0,0 +1,67 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+
+#include "google/cloud/bigquery/v2/minimal/internal/dataset_client.h"
+#include "google/cloud/options.h"
+#include "google/cloud/credentials.h"
+#include "google/cloud/internal/getenv.h"
+
+#include "google/cloud/odbc/integration_tests/testing_util/status_matchers.h"
+
+namespace google {
+namespace cloud {
+namespace odbc_bigquery_v2_tests {
+
+  using google::cloud::internal::GetEnv;
+  using bigquery_v2_minimal_internal::DatasetClient;
+  using bigquery_v2_minimal_internal::MakeDatasetConnection;
+  using bigquery_v2_minimal_internal::GetDatasetRequest;
+
+  void getDataset(Options options) {
+    auto dataset_client = DatasetClient(MakeDatasetConnection(std::move(options)));
+    auto project_id_optional = GetEnv("CPP_BIGQUERY_ODBC_TEST_GOOGLE_CLOUD_PROJECT");
+    auto dataset_id_optional = GetEnv("CPP_BIGQUERY_ODBC_TEST_BIGQUERY_DATASET");
+    ASSERT_TRUE(project_id_optional.has_value());
+    ASSERT_TRUE(dataset_id_optional.has_value());
+    GetDatasetRequest request;
+    request.set_project_id(project_id_optional.value());
+    request.set_dataset_id(dataset_id_optional.value());
+
+    auto dataset = dataset_client.GetDataset(request);
+
+    ASSERT_STATUS_OK(dataset);
+  }
+
+  TEST(GetDataset, UserAccountAuth) {
+    std::string path_to_file_with_credentials = GetEnv("CPP_BIGQUERY_ODBC_TEST_USER_ACCOUNT_ACCOUNT_KEY").value_or("");
+    ASSERT_NE(path_to_file_with_credentials, "");
+    setenv("GOOGLE_APPLICATION_CREDENTIALS", path_to_file_with_credentials.c_str(), 1);
+    auto options = google::cloud::Options{}.set<google::cloud::UnifiedCredentialsOption>(
+        google::cloud::MakeGoogleDefaultCredentials());
+    getDataset(options);
+  }
+
+  TEST(GetDataset, ServiceAccountAuthWithClientId) {
+    std::string path_to_file_with_credentials = GetEnv("CPP_BIGQUERY_ODBC_TEST_CLIENT_ID_ACCOUNT_KEY").value_or("");
+    ASSERT_NE(path_to_file_with_credentials, "");
+    setenv("GOOGLE_APPLICATION_CREDENTIALS", path_to_file_with_credentials.c_str(), 1);
+    auto options = google::cloud::Options{}.set<google::cloud::UnifiedCredentialsOption>(
+        google::cloud::MakeGoogleDefaultCredentials());
+    getDataset(options);
+  }
+}
+}
+}
