fd4db8564aa95a301927c099f1db4bdd9549133e
meilakh <meilakh@google.com>
2023-11-10 12:24:29 +0000

    feat: add 'get_job' tests

---
+++ google/cloud/odbc/integration_tests/apis/get_job_test.cc
@@ -0,0 +1,261 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gmock/gmock.h>
+#include "absl/strings/str_cat.h"
+
+#include "google/cloud/bigquery/v2/minimal/internal/job_client.h"
+#include "google/cloud/internal/getenv.h"
+
+#include "google/cloud/odbc/integration_tests/testing_util/authentication.h"
+#include "google/cloud/odbc/integration_tests/testing_util/status_matchers.h"
+
+namespace google {
+namespace cloud {
+namespace odbc_bigquery_v2_tests {
+
+using google::cloud::internal::GetEnv;
+using google::cloud::odbc_testing_util_internal::StatusIs;
+using google::cloud::odbc_testing_util_internal::CreateServiceAccountAuthWithClientIdAuthentication;
+using google::cloud::odbc_testing_util_internal::CreateUserAccountAuthentication;
+using ::testing::HasSubstr;
+using bigquery_v2_minimal_internal::JobClient;
+using bigquery_v2_minimal_internal::MakeBigQueryJobConnection;
+using bigquery_v2_minimal_internal::InsertJobRequest;
+using bigquery_v2_minimal_internal::Job;
+using bigquery_v2_minimal_internal::JobConfiguration;
+using bigquery_v2_minimal_internal::JobConfigurationQuery;
+using bigquery_v2_minimal_internal::GetJobRequest;
+
+TEST(GetJob, ServiceAccountAuthWithClientId) {
+  // First we create a job, so later we could 'get' it
+  auto options = CreateServiceAccountAuthWithClientIdAuthentication();
+  ASSERT_STATUS_OK(options);
+  auto job_client = JobClient(MakeBigQueryJobConnection(std::move(options.value())));
+  auto project_id_optional = GetEnv("CPP_BIGQUERY_ODBC_TEST_GOOGLE_CLOUD_PROJECT");
+  auto dataset_id_optional = GetEnv("CPP_BIGQUERY_ODBC_TEST_BIGQUERY_DATASET");
+  auto table_name_optional = GetEnv("CPP_BIGQUERY_ODBC_TEST_TABLE_NAME");
+  ASSERT_TRUE(project_id_optional.has_value());
+  ASSERT_TRUE(dataset_id_optional.has_value());
+  ASSERT_TRUE(table_name_optional.has_value());
+
+  Job job;
+  JobConfiguration job_configuration;
+  JobConfigurationQuery job_configuration_query;
+  std::string table_name = absl::StrCat(dataset_id_optional.value(), ".", table_name_optional.value());
+  job_configuration_query.query = absl::StrCat("SELECT * FROM ", table_name);
+  job_configuration.query = job_configuration_query;
+  job.configuration = job_configuration;
+  InsertJobRequest request;
+  request.set_project_id(project_id_optional.value());
+  request.set_job(job);
+
+  request.set_json_filter_keys({"statistics", "status", "labels", "destinationTable",
+                                "maximumBytesBilled", "userDefinedFunctionResources", "defaultDataset",
+                                "schemaUpdateOptions", "timePartitioning", "rangePartitioning",
+                                "clustering", "destinationEncryptionConfiguration", "scriptOptions",
+                                "connectionProperties", "systemVariables", "structTypes",
+                                "structValues", "location"});
+
+  auto job_response = job_client.InsertJob(request);
+
+  ASSERT_STATUS_OK(job_response);
+
+  // Getting previous Job
+  std::string job_id = job_response.value().job_reference.job_id;
+  GetJobRequest get_job_request;
+  get_job_request.set_project_id(project_id_optional.value());
+  get_job_request.set_job_id(job_id);
+
+  auto get_job_response = job_client.GetJob(get_job_request);
+
+  ASSERT_STATUS_OK(get_job_response);
+  EXPECT_EQ(get_job_response.value().status.state, "DONE");
+}
+
+TEST(GetJob, DifferentAccount) {
+  // First we create a job, so later we could 'get' it
+  auto options = CreateServiceAccountAuthWithClientIdAuthentication();
+  ASSERT_STATUS_OK(options);
+  auto job_client = JobClient(MakeBigQueryJobConnection(std::move(options.value())));
+  auto project_id_optional = GetEnv("CPP_BIGQUERY_ODBC_TEST_GOOGLE_CLOUD_PROJECT");
+  auto dataset_id_optional = GetEnv("CPP_BIGQUERY_ODBC_TEST_BIGQUERY_DATASET");
+  auto table_name_optional = GetEnv("CPP_BIGQUERY_ODBC_TEST_TABLE_NAME");
+  ASSERT_TRUE(project_id_optional.has_value());
+  ASSERT_TRUE(dataset_id_optional.has_value());
+  ASSERT_TRUE(table_name_optional.has_value());
+
+  Job job;
+  JobConfiguration job_configuration;
+  JobConfigurationQuery job_configuration_query;
+  std::string table_name = absl::StrCat(dataset_id_optional.value(), ".", table_name_optional.value());
+  job_configuration_query.query = absl::StrCat("SELECT * FROM ", table_name);
+  job_configuration.query = job_configuration_query;
+  job.configuration = job_configuration;
+  InsertJobRequest request;
+  request.set_project_id(project_id_optional.value());
+  request.set_job(job);
+
+  request.set_json_filter_keys({"statistics", "status", "labels", "destinationTable",
+                                "maximumBytesBilled", "userDefinedFunctionResources", "defaultDataset",
+                                "schemaUpdateOptions", "timePartitioning", "rangePartitioning",
+                                "clustering", "destinationEncryptionConfiguration", "scriptOptions",
+                                "connectionProperties", "systemVariables", "structTypes",
+                                "structValues", "location"});
+
+  auto job_response = job_client.InsertJob(request);
+
+  ASSERT_STATUS_OK(job_response);
+
+  // Getting previous Job with another account
+  auto options_with_user_account = CreateUserAccountAuthentication();
+  ASSERT_STATUS_OK(options_with_user_account);
+  auto job_client_with_user_account = JobClient(MakeBigQueryJobConnection(std::move(options_with_user_account.value())));
+  std::string job_id = job_response.value().job_reference.job_id;
+  GetJobRequest get_job_request;
+  get_job_request.set_project_id(project_id_optional.value());
+  get_job_request.set_job_id(job_id);
+
+  auto get_job_response = job_client_with_user_account.GetJob(get_job_request);
+
+  ASSERT_STATUS_OK(get_job_response);
+  EXPECT_EQ(get_job_response.value().status.state, "DONE");
+}
+
+TEST(GetJob, WrongLocation) {
+  // First we create a job, so later we could 'get' it
+  auto options = CreateServiceAccountAuthWithClientIdAuthentication();
+  ASSERT_STATUS_OK(options);
+  auto job_client = JobClient(MakeBigQueryJobConnection(std::move(options.value())));
+  auto project_id_optional = GetEnv("CPP_BIGQUERY_ODBC_TEST_GOOGLE_CLOUD_PROJECT");
+  auto dataset_id_optional = GetEnv("CPP_BIGQUERY_ODBC_TEST_BIGQUERY_DATASET");
+  auto table_name_optional = GetEnv("CPP_BIGQUERY_ODBC_TEST_TABLE_NAME");
+  ASSERT_TRUE(project_id_optional.has_value());
+  ASSERT_TRUE(dataset_id_optional.has_value());
+  ASSERT_TRUE(table_name_optional.has_value());
+
+  Job job;
+  JobConfiguration job_configuration;
+  JobConfigurationQuery job_configuration_query;
+  std::string table_name = absl::StrCat(dataset_id_optional.value(), ".", table_name_optional.value());
+  job_configuration_query.query = absl::StrCat("SELECT * FROM ", table_name);
+  job_configuration.query = job_configuration_query;
+  job.configuration = job_configuration;
+  InsertJobRequest request;
+  request.set_project_id(project_id_optional.value());
+  request.set_job(job);
+
+  request.set_json_filter_keys({"statistics", "status", "labels", "destinationTable",
+                                "maximumBytesBilled", "userDefinedFunctionResources", "defaultDataset",
+                                "schemaUpdateOptions", "timePartitioning", "rangePartitioning",
+                                "clustering", "destinationEncryptionConfiguration", "scriptOptions",
+                                "connectionProperties", "systemVariables", "structTypes",
+                                "structValues", "location"});
+
+  auto job_response = job_client.InsertJob(request);
+
+  ASSERT_STATUS_OK(job_response);
+
+  // Getting previous Job
+  std::string job_id = job_response.value().job_reference.job_id;
+  GetJobRequest get_job_request;
+  get_job_request.set_project_id(project_id_optional.value());
+  get_job_request.set_job_id(job_id);
+  get_job_request.set_location("asia-south2");
+
+  auto get_job_response = job_client.GetJob(get_job_request);
+
+  EXPECT_THAT(get_job_response, StatusIs(StatusCode::kNotFound, HasSubstr("Not found: Job")));
+}
+
+TEST(GetJob, LocationNotExist) {
+  // First we create a job, so later we could 'get' it
+  auto options = CreateServiceAccountAuthWithClientIdAuthentication();
+  ASSERT_STATUS_OK(options);
+  auto job_client = JobClient(MakeBigQueryJobConnection(std::move(options.value())));
+  auto project_id_optional = GetEnv("CPP_BIGQUERY_ODBC_TEST_GOOGLE_CLOUD_PROJECT");
+  auto dataset_id_optional = GetEnv("CPP_BIGQUERY_ODBC_TEST_BIGQUERY_DATASET");
+  auto table_name_optional = GetEnv("CPP_BIGQUERY_ODBC_TEST_TABLE_NAME");
+  ASSERT_TRUE(project_id_optional.has_value());
+  ASSERT_TRUE(dataset_id_optional.has_value());
+  ASSERT_TRUE(table_name_optional.has_value());
+
+  Job job;
+  JobConfiguration job_configuration;
+  JobConfigurationQuery job_configuration_query;
+  std::string table_name = absl::StrCat(dataset_id_optional.value(), ".", table_name_optional.value());
+  job_configuration_query.query = absl::StrCat("SELECT * FROM ", table_name);
+  job_configuration.query = job_configuration_query;
+  job.configuration = job_configuration;
+  InsertJobRequest request;
+  request.set_project_id(project_id_optional.value());
+  request.set_job(job);
+
+  request.set_json_filter_keys({"statistics", "status", "labels", "destinationTable",
+                                "maximumBytesBilled", "userDefinedFunctionResources", "defaultDataset",
+                                "schemaUpdateOptions", "timePartitioning", "rangePartitioning",
+                                "clustering", "destinationEncryptionConfiguration", "scriptOptions",
+                                "connectionProperties", "systemVariables", "structTypes",
+                                "structValues", "location"});
+
+  auto job_response = job_client.InsertJob(request);
+
+  ASSERT_STATUS_OK(job_response);
+
+  // Getting previous Job
+  std::string job_id = job_response.value().job_reference.job_id;
+  GetJobRequest get_job_request;
+  get_job_request.set_project_id(project_id_optional.value());
+  get_job_request.set_job_id(job_id);
+  get_job_request.set_location("Not_existing_location");
+
+  auto get_job_response = job_client.GetJob(get_job_request);
+
+  EXPECT_THAT(get_job_response, StatusIs(StatusCode::kInvalidArgument, HasSubstr("Invalid value for location")));
+}
+
+TEST(GetJob, JobNotExist) {
+  auto options = CreateServiceAccountAuthWithClientIdAuthentication();
+  ASSERT_STATUS_OK(options);
+  auto job_client = JobClient(MakeBigQueryJobConnection(std::move(options.value())));
+  auto project_id_optional = GetEnv("CPP_BIGQUERY_ODBC_TEST_GOOGLE_CLOUD_PROJECT");
+  ASSERT_TRUE(project_id_optional.has_value());
+
+  GetJobRequest get_job_request;
+  get_job_request.set_project_id(project_id_optional.value());
+  get_job_request.set_job_id("Not_existing_job");
+
+  auto get_job_response = job_client.GetJob(get_job_request);
+
+  EXPECT_THAT(get_job_response, StatusIs(StatusCode::kNotFound, HasSubstr("Not found: Job")));
+}
+
+TEST(GetJob, ProjectNotExist) {
+  auto options = CreateServiceAccountAuthWithClientIdAuthentication();
+  ASSERT_STATUS_OK(options);
+  auto job_client = JobClient(MakeBigQueryJobConnection(std::move(options.value())));
+  auto project_id_optional = GetEnv("CPP_BIGQUERY_ODBC_TEST_GOOGLE_CLOUD_PROJECT");
+  ASSERT_TRUE(project_id_optional.has_value());
+
+  GetJobRequest get_job_request;
+  get_job_request.set_project_id("not-existing-project");
+  get_job_request.set_job_id("Not_existing_job");
+
+  auto get_job_response = job_client.GetJob(get_job_request);
+
+  EXPECT_THAT(get_job_response, StatusIs(StatusCode::kNotFound, HasSubstr("Not found: Project")));
+}
+}
+}
+}
