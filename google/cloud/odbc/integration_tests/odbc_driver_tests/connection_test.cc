aa737d91de147f25abe1205927a8563f52c6f9e3
meilakh <meilakh@google.com>
2024-03-22 14:36:07 +0000

    feat: add integration tests

--- google/cloud/odbc/integration_tests/odbc_driver_tests/connection_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/connection_test.cc
@@ -424,6 +424,34 @@ TEST(DriverInfoTest, SQLGetInfo) {
   EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
 }
 
+TEST(ConnectionTest, SQLSetConnectAttr) {
+  SQLCHAR buf[256] = "test";
+  auto conn = std::make_shared<ODBCHandles>();
+  EXPECT_EQ(ConnectDsn(kDefaultDataSource, conn), SQL_SUCCESS);
+
+  auto status = SQLSetConnectAttr(conn->hdbc, SQL_ATTR_CURRENT_CATALOG, (SQLPOINTER)buf, 2);
+  CheckError(status, "SQLSetConnectAttr", conn);
+
+  std::string expected = "te";
+
+  buf[0] = '0';
+  std::string buffer = reinterpret_cast<char*>(buf);
+  EXPECT_EQ("0est", buffer);
+
+  SQLCHAR output[256];
+  SQLINTEGER length;
+  status = SQLGetConnectAttr(conn->hdbc, SQL_ATTR_CURRENT_CATALOG,
+                                  (SQLPOINTER)output,
+                                  256, &length);
+  CheckError(status, "SQLGetConnectAttr", conn);
+
+  std::string actual = reinterpret_cast<char*>(output);
+  EXPECT_EQ(expected, actual);
+  EXPECT_EQ(expected.size(), length);
+
+  EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
+}
+
 // This test is temporarily disabled till this issue is fixed for the driver
 TEST(ConnectionTest, DISABLED_SQLGetConnectAttr) {
   srand(time(NULL));
