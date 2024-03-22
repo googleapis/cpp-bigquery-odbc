0d8863f68d7bcdeb83973e050c59a06d93f25636
meilakh <meilakh@google.com>
2024-03-22 14:50:12 +0000

    feat: add integration tests

--- google/cloud/odbc/integration_tests/odbc_driver_tests/connection_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/connection_test.cc
@@ -424,12 +424,13 @@ TEST(DriverInfoTest, SQLGetInfo) {
   EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
 }
 
-TEST(ConnectionTest, SQLSetConnectAttr) {
+TEST(ConnectionTest, SQLSetConnectAttr_String) {
   SQLCHAR buf[256] = "test";
   auto conn = std::make_shared<ODBCHandles>();
   EXPECT_EQ(ConnectDsn(kDefaultDataSource, conn), SQL_SUCCESS);
 
-  auto status = SQLSetConnectAttr(conn->hdbc, SQL_ATTR_CURRENT_CATALOG, (SQLPOINTER)buf, 2);
+  auto status = SQLSetConnectAttr(conn->hdbc, SQL_ATTR_CURRENT_CATALOG,
+                                  (SQLPOINTER)buf, 2);
   CheckError(status, "SQLSetConnectAttr", conn);
 
   std::string expected = "te";
@@ -441,13 +442,37 @@ TEST(ConnectionTest, SQLSetConnectAttr) {
   SQLCHAR output[256];
   SQLINTEGER length;
   status = SQLGetConnectAttr(conn->hdbc, SQL_ATTR_CURRENT_CATALOG,
-                                  (SQLPOINTER)output,
-                                  256, &length);
+                             (SQLPOINTER)output, 256, &length);
   CheckError(status, "SQLGetConnectAttr", conn);
 
   std::string actual = reinterpret_cast<char*>(output);
   EXPECT_EQ(expected, actual);
   EXPECT_EQ(expected.size(), length);
+
+  EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
+}
+
+TEST(ConnectionTest, SQLSetConnectAttr_Integer) {
+  SQLULEN buf = SQL_ASYNC_ENABLE_ON;
+  auto conn = std::make_shared<ODBCHandles>();
+  EXPECT_EQ(ConnectDsn(kDefaultDataSource, conn), SQL_SUCCESS);
+
+  auto status =
+      SQLSetConnectAttr(conn->hdbc, SQL_ATTR_ASYNC_ENABLE, (SQLPOINTER)buf, 2);
+  CheckError(status, "SQLSetConnectAttr", conn);
+
+  SQLULEN expected = buf;
+
+  auto* buf_ptr = &buf;
+  *buf_ptr = 222;
+  EXPECT_EQ(222, buf);
+
+  SQLULEN output = 0;
+  status =
+      SQLGetConnectAttr(conn->hdbc, SQL_ATTR_ASYNC_ENABLE, &output, 256, NULL);
+  CheckError(status, "SQLGetConnectAttr", conn);
+
+  EXPECT_EQ(expected, output);
 
   EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
 }
