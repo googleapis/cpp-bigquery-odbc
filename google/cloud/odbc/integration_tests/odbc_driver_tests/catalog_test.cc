e9e579b5eb9cf2b4d009e189853d0987ba968adf
Jaya Srinivasan <jsrinivasan@google.com>
2024-04-23 19:06:53 +0000

    impl(bq_driver): Address remaining code review feedback

--- google/cloud/odbc/integration_tests/odbc_driver_tests/catalog_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/catalog_test.cc
@@ -18,9 +18,26 @@
 namespace google::cloud::odbc_tests {
 
 static std::string const kCatalogFnsDataset = "ODBC_TEST_DATASET_CATALOG_FNS";
-static std::string const kCatalogDatasetTableWithPK = "ODBC_TEST_TABLE_WITH_PK";
+static std::string const kCatalogDatasetTableWithPK =
+    "ODBC_SQLPrimaryKeys_TABLE_WITH_PK";
 static std::string const kCatalogDatasetTableWithoutPK =
-    "ODBC_TEST_TABLE_WITHOUT_PK";
+    "ODBC_SQLPrimaryKeys_TABLE_WITHOUT_PK";
+static std::string const kCatalogDatasetTableWithPKFull =
+    kCatalogFnsDataset + "." + kCatalogDatasetTableWithPK;
+static std::string const kCatalogDatasetTableWithoutPKFull =
+    kCatalogFnsDataset + "." + kCatalogDatasetTableWithoutPK;
+
+static std::string const kTableWithPKSchema =
+    "CREATE IF NOT EXISTS TABLE " + kCatalogDatasetTableWithPKFull +
+    " "
+    "(StringField STRING, IntField INT64, FloatField FLOAT64) "
+    "PRIMARY KEY (StringField, IntField) NOT ENFORCED";
+
+static std::string const kTableWithOutPKSchema =
+    "CREATE IF NOT EXISTS TABLE " + kCatalogDatasetTableWithoutPKFull +
+    " "
+    "ODBC_TEST_DATASET_CATALOG_FNS.ODBC_SQLPrimaryKeys_TABLE_WITHOUT_PK "
+    "(StringField STRING, IntField INT64, FloatField FLOAT64)";
 // This preprocessor flag is used to disable tests for unimplemented bq_driver
 // ODBC APIs
 #ifndef BQ_DRIVER_INTEGRATION_TESTS
@@ -150,7 +167,10 @@ TEST(CatalogTest, SQLTablesA) {
 // SQLPrimaryKeys is not implemented by Simba
 TEST(CatalogTest, SQLPrimaryKeys_TableWithPrimaryKeys) {
   auto conn = std::make_shared<ODBCHandles>();
+  // Create table if not exists.
   EXPECT_EQ(Connect(kDefaultConnectionString, conn), SQL_SUCCESS);
+  CreateTableDirect(conn, kTableWithPKSchema);
+
   // Use existing dataset and table created with primary keys.
   // We are not creating and dropping tables. This existing
   // table resource can be reused for other catalog functions as well.
@@ -166,7 +186,9 @@ TEST(CatalogTest, SQLPrimaryKeys_TableWithPrimaryKeys) {
 
 TEST(CatalogTest, SQLPrimaryKeys_TableWithoutPrimaryKeys) {
   auto conn = std::make_shared<ODBCHandles>();
+  // Create table if not exists.
   EXPECT_EQ(Connect(kDefaultConnectionString, conn), SQL_SUCCESS);
+  CreateTableDirect(conn, kTableWithOutPKSchema);
   // Use existing dataset and table created with primary keys.
   // We are not creating and dropping tables. This existing
   // table resource can be reused for other catalog functions as well.
@@ -178,7 +200,9 @@ TEST(CatalogTest, SQLPrimaryKeys_TableWithoutPrimaryKeys) {
 
 TEST(CatalogTest, ANSI_SQLPrimaryKeys_TableWithPrimaryKeys) {
   auto conn = std::make_shared<ODBCHandles>();
+  // Create table if not exists.
   EXPECT_EQ(Connect(kDefaultConnectionString, conn, true), SQL_SUCCESS);
+  CreateTableDirect(conn, kTableWithPKSchema, true);
   // Use existing dataset and table created with primary keys.
   // We are not creating and dropping tables. This existing
   // table resource can be reused for other catalog functions as well.
@@ -194,7 +218,9 @@ TEST(CatalogTest, ANSI_SQLPrimaryKeys_TableWithPrimaryKeys) {
 
 TEST(CatalogTest, ANSI_SQLPrimaryKeys_TableWithoutPrimaryKeys) {
   auto conn = std::make_shared<ODBCHandles>();
+  // Create table if not exists.
   EXPECT_EQ(Connect(kDefaultConnectionString, conn, true), SQL_SUCCESS);
+  CreateTableDirect(conn, kTableWithOutPKSchema, true);
   // Use existing dataset and table created with primary keys.
   // We are not creating and dropping tables. This existing
   // table resource can be reused for other catalog functions as well.
