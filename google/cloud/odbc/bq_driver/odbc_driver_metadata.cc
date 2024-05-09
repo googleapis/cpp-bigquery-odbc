1cb7598e7c783272abc36194e7021852ea81691e
Jaya Srinivasan <jsrinivasan@google.com>
2024-05-09 19:19:50 +0000

    impl(bq_driver): addressed clang-tidy issues

--- google/cloud/odbc/bq_driver/odbc_driver_metadata.cc
+++ google/cloud/odbc/bq_driver/odbc_driver_metadata.cc
@@ -238,9 +238,10 @@ SQLRETURN SQLGetTypeInfoInternal(SQLHSTMT /* stmt_handle */,
   return SQL_SUCCESS;
 }
 
-SQLRETURN SQLPrimaryKeys(SQLHSTMT stmt_handle, SQLCHAR* catalog_name,
-                         SQLSMALLINT catalog_name_len, SQLCHAR* schema_name,
-                         SQLSMALLINT schema_name_len, SQLCHAR* table_name,
+SQLRETURN SQLPrimaryKeys(SQLHSTMT stmt_handle, SQLCHAR* const catalog_name,
+                         SQLSMALLINT catalog_name_len,
+                         SQLCHAR* const schema_name,
+                         SQLSMALLINT schema_name_len, SQLCHAR* const table_name,
                          SQLSMALLINT table_name_len) {
   SQLRETURN rc = SQL_SUCCESS;
   StatusRecordOr<StatementHandle*> handle_result =
