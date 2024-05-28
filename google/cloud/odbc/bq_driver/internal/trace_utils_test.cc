105edeb24da6f30e14b377496f4d8bcd93e078b7
Neeraj Dwivedi <neeraj.dwivedi@37848692.corp-partner.google.com>
2024-05-28 13:15:20 +0530

    test(unit_test): Added More Test Cases For Trace Log File

--- google/cloud/odbc/bq_driver/internal/trace_utils_test.cc
+++ google/cloud/odbc/bq_driver/internal/trace_utils_test.cc
@@ -57,13 +57,12 @@ TEST(TraceLoggingFile, TraceOptionsFromConfigTraceEnabled) {
   (*test_opts_file)->trace_file.close();
 }
 
-TEST(TraceLogginfFile, TraceOptionsFromConfigLogfileClosed) {
+TEST(TraceLoggingFile, TraceOptionsFromConfigLogfileClosed) {
   auto config_sections = std::make_shared<Sections>(kConfigSections1);
   StatusRecordOr<std::shared_ptr<TraceOptions>> test_opts_file =
       TraceOptions::CreateTraceOptionsFile(config_sections);
   ASSERT_STATUS_RECORD_OK(test_opts_file);
 
-  std::string fmt1 = FormatSqlReturn(1);
   EXPECT_EQ("SQLAllocHandle_Exit\t\tSQLRETURN, 1\n",
             ExitInternal("SQLAllocHandle_Exit", 1, *(*test_opts_file)));
 
@@ -181,6 +180,18 @@ TEST(TraceLoggingFile, GetTraceOptionFromConfigTraceFilePresent) {
   EXPECT_EQ(1, (*test_option)->log_level);
 
   (*test_option)->trace_file.close();
+}
+
+TEST(TraceLogging, TraceLoggingDisabled) {
+  auto config_sections = std::make_shared<Sections>(kConfigSections2);
+  StatusRecordOr<std::shared_ptr<TraceOptions>> test_opts_file =
+      TraceOptions::CreateTraceOptionsFile(config_sections);
+
+  std::shared_ptr<TraceOptions> test_trace_opts_console =
+      *(TraceOptions::CreateTraceOptionsConsole(false, 0));
+
+  EXPECT_FALSE((*test_opts_file)->logging_enabled);
+  EXPECT_FALSE((test_trace_opts_console)->logging_enabled);
 }
 
 TEST(TraceLoggingConsole, BasicODBCTypes) {
