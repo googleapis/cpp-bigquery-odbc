3f862723233376c46197a0f42e9ff2d84f1b8a0b
Sachin Purohit <sachinpurohit@google.com>
2024-06-04 02:15:06 +0000

    impl(bq_driver): implementation of SQLGetTypeInfo and enabling integration tests for it

--- google/cloud/odbc/bq_driver/odbc_driver_metadata.cc
+++ google/cloud/odbc/bq_driver/odbc_driver_metadata.cc
@@ -22,27 +22,36 @@
 
 namespace google::cloud::odbc_bq_driver {
 
-using ::google::cloud::odbc_bq_driver_internal::ConnectionHandle;
-using ::google::cloud::odbc_bq_driver_internal::DSResults;
-using ::google::cloud::odbc_bq_driver_internal::FetchPrimaryKeysFromDataSource;
-using ::google::cloud::odbc_bq_driver_internal::IsFunctionIdOdbc2;
-using ::google::cloud::odbc_bq_driver_internal::IsFunctionIdOdbc3;
-using ::google::cloud::odbc_bq_driver_internal::kSqlApiAllFuncsSize;
-using ::google::cloud::odbc_bq_driver_internal::kTraceOption;
-using ::google::cloud::odbc_bq_driver_internal::PopulateSupportedODBC2Functions;
-using ::google::cloud::odbc_bq_driver_internal::PopulateSupportedODBC3Functions;
-using ::google::cloud::odbc_bq_driver_internal::ProcessQueryResults;
-using ::google::cloud::odbc_bq_driver_internal::ResultSet;
-using ::google::cloud::odbc_bq_driver_internal::SQLGetInfoBitmask;
-using ::google::cloud::odbc_bq_driver_internal::SQLGetInfoSqlChar;
-using ::google::cloud::odbc_bq_driver_internal::SQLGetInfoSqlUInt;
-using ::google::cloud::odbc_bq_driver_internal::SQLGetInfoSqlUSmallInt;
-using ::google::cloud::odbc_bq_driver_internal::StatementHandle;
-using ::google::cloud::odbc_bq_driver_internal::StmtStates;
-using ::google::cloud::odbc_bq_driver_internal::SupportedInfoType;
-using ::google::cloud::odbc_bq_driver_internal::TraceOptions;
-using ::google::cloud::odbc_bq_driver_internal::TracePrintInternal;
-using ::google::cloud::odbc_bq_driver_internal::UnSupportedInfoType;
+using google::cloud::odbc_bq_driver_internal::ArithmeticToDSValue;
+using google::cloud::odbc_bq_driver_internal::BQDataType;
+using google::cloud::odbc_bq_driver_internal::ColumnSchema;
+using google::cloud::odbc_bq_driver_internal::ConnectionHandle;
+using google::cloud::odbc_bq_driver_internal::DSResults;
+using google::cloud::odbc_bq_driver_internal::DSRow;
+using google::cloud::odbc_bq_driver_internal::DSValue;
+using google::cloud::odbc_bq_driver_internal::FetchPrimaryKeysFromDataSource;
+using google::cloud::odbc_bq_driver_internal::IsFunctionIdOdbc2;
+using google::cloud::odbc_bq_driver_internal::IsFunctionIdOdbc3;
+using google::cloud::odbc_bq_driver_internal::kSqlApiAllFuncsSize;
+using google::cloud::odbc_bq_driver_internal::kSqlToBqDataTypes;
+using google::cloud::odbc_bq_driver_internal::kTraceOption;
+using google::cloud::odbc_bq_driver_internal::PopulateSupportedODBC2Functions;
+using google::cloud::odbc_bq_driver_internal::PopulateSupportedODBC3Functions;
+using google::cloud::odbc_bq_driver_internal::ProcessQueryResults;
+using google::cloud::odbc_bq_driver_internal::ResultSet;
+using google::cloud::odbc_bq_driver_internal::RowSchema;
+using google::cloud::odbc_bq_driver_internal::SQLGetInfoBitmask;
+using google::cloud::odbc_bq_driver_internal::SQLGetInfoSqlChar;
+using google::cloud::odbc_bq_driver_internal::SQLGetInfoSqlUInt;
+using google::cloud::odbc_bq_driver_internal::SQLGetInfoSqlUSmallInt;
+using google::cloud::odbc_bq_driver_internal::StatementHandle;
+using google::cloud::odbc_bq_driver_internal::StmtStates;
+using google::cloud::odbc_bq_driver_internal::StringToDSValue;
+using google::cloud::odbc_bq_driver_internal::SupportedInfoType;
+using google::cloud::odbc_bq_driver_internal::TraceOptions;
+using google::cloud::odbc_bq_driver_internal::TracePrintInternal;
+using google::cloud::odbc_bq_driver_internal::TypeInfoRow;
+using google::cloud::odbc_bq_driver_internal::UnSupportedInfoType;
 using google::cloud::odbc_internal::SQLStates;
 using google::cloud::odbc_internal::StatusRecord;
 using google::cloud::odbc_internal::StatusRecordOr;
@@ -238,8 +247,226 @@ SQLRETURN SQLGetInfoInternal(SQLHDBC connection_handle, SQLUSMALLINT info_type,
   return status_record.CalculateReturnCode();
 }
 
-SQLRETURN SQLGetTypeInfoInternal(SQLHSTMT /* stmt_handle */,
-                                 SQLSMALLINT /* data_type */) {
+DSRow CreateDSRowFromTypeInfo(TypeInfoRow& type_info) {
+  DSRow ds_row;
+
+  DSValue type_name;
+  StringToDSValue(type_info.type_name, type_name);
+  ds_row.emplace_back(type_name);
+
+  DSValue data_type;
+  ArithmeticToDSValue<SQLBIGINT>(static_cast<SQLBIGINT>(type_info.data_type),
+                                 data_type);
+  ds_row.emplace_back(data_type);
+
+  DSValue col_size;
+  ArithmeticToDSValue<SQLBIGINT>(static_cast<SQLBIGINT>(type_info.col_size),
+                                 col_size);
+  ds_row.emplace_back(col_size);
+
+  if (type_info.literal_prefix) {
+    DSValue literal_prefix;
+    StringToDSValue(type_info.literal_prefix, literal_prefix);
+    ds_row.emplace_back(literal_prefix);
+  } else {
+    ds_row.emplace_back();
+  }
+
+  if (type_info.literal_suffix) {
+    DSValue literal_suffix;
+    StringToDSValue(type_info.literal_suffix, literal_suffix);
+    ds_row.emplace_back(literal_suffix);
+  } else {
+    ds_row.emplace_back();
+  }
+
+  if (type_info.create_params) {
+    DSValue create_params;
+    StringToDSValue(type_info.create_params, create_params);
+    ds_row.emplace_back(create_params);
+  } else {
+    ds_row.emplace_back();
+  }
+
+  DSValue nullable;
+  ArithmeticToDSValue<SQLBIGINT>(type_info.nullable, nullable);
+  ds_row.emplace_back(nullable);
+
+  DSValue case_sensitive;
+  ArithmeticToDSValue<SQLBIGINT>(type_info.case_sensitive, case_sensitive);
+  ds_row.emplace_back(case_sensitive);
+
+  DSValue searchable;
+  ArithmeticToDSValue<SQLBIGINT>(type_info.searchable, searchable);
+  ds_row.emplace_back(searchable);
+
+  DSValue unsigned_attribute;
+  ArithmeticToDSValue<SQLBIGINT>(type_info.unsigned_attribute,
+                                 unsigned_attribute);
+  ds_row.emplace_back(unsigned_attribute);
+
+  DSValue fixed_prec_scale;
+  ArithmeticToDSValue<SQLBIGINT>(type_info.fixed_prec_scale, fixed_prec_scale);
+  ds_row.emplace_back(fixed_prec_scale);
+
+  DSValue auto_unique_value;
+  ArithmeticToDSValue<SQLBIGINT>(type_info.auto_unique_value,
+                                 auto_unique_value);
+  ds_row.emplace_back(auto_unique_value);
+
+  DSValue local_type_name;
+  StringToDSValue(type_info.local_type_name, local_type_name);
+  ds_row.emplace_back(local_type_name);
+
+  DSValue minimum_scale;
+  ArithmeticToDSValue<SQLBIGINT>(type_info.minimum_scale, minimum_scale);
+  ds_row.emplace_back(minimum_scale);
+
+  DSValue maximum_scale;
+  ArithmeticToDSValue<SQLBIGINT>(type_info.maximum_scale, maximum_scale);
+  ds_row.emplace_back(maximum_scale);
+
+  DSValue sql_data_type;
+  ArithmeticToDSValue<SQLBIGINT>(type_info.sql_data_type, sql_data_type);
+  ds_row.emplace_back(sql_data_type);
+
+  DSValue sql_datetime_sub;
+  ArithmeticToDSValue<SQLBIGINT>(type_info.sql_datetime_sub, sql_datetime_sub);
+  ds_row.emplace_back(sql_datetime_sub);
+
+  DSValue num_prec_radix;
+  ArithmeticToDSValue<SQLBIGINT>(type_info.num_prec_radix, num_prec_radix);
+  ds_row.emplace_back(num_prec_radix);
+
+  DSValue interval_precision;
+  ArithmeticToDSValue<SQLBIGINT>(type_info.interval_precision,
+                                 interval_precision);
+  ds_row.emplace_back(interval_precision);
+
+  return ds_row;
+}
+
+void CreateTypeInfoRowSchema(ResultSet& result_set) {
+  RowSchema& row_schema = result_set.row_schema;
+  ColumnSchema col_schema;
+
+  // Schema for type_name
+  col_schema.col_index = 0;
+  col_schema.col_type = BQDataType::kString;
+  row_schema.push_back(col_schema);
+
+  // Schema for data_type
+  col_schema.col_index++;
+  col_schema.col_type = BQDataType::kInt64;
+  row_schema.push_back(col_schema);
+
+  // Schema for col_size
+  col_schema.col_index++;
+  col_schema.col_type = BQDataType::kInt64;
+  row_schema.push_back(col_schema);
+
+  // Schema for literal_prefix
+  col_schema.col_index++;
+  col_schema.col_type = BQDataType::kString;
+  row_schema.push_back(col_schema);
+
+  // Schema for literal_suffix
+  col_schema.col_index++;
+  row_schema.push_back(col_schema);
+
+  // Schema for create_params
+  col_schema.col_index++;
+  row_schema.push_back(col_schema);
+
+  // Schema for nullable
+  col_schema.col_index++;
+  col_schema.col_type = BQDataType::kInt64;
+  row_schema.push_back(col_schema);
+
+  // Schema for case_sensitive
+  col_schema.col_index++;
+  row_schema.push_back(col_schema);
+
+  // Schema for searchable
+  col_schema.col_index++;
+  row_schema.push_back(col_schema);
+
+  // Schema for unsigned_attribute
+  col_schema.col_index++;
+  row_schema.push_back(col_schema);
+
+  // Schema for fixed_prec_scale
+  col_schema.col_index++;
+  row_schema.push_back(col_schema);
+
+  // Schema for auto_unique_value
+  col_schema.col_index++;
+  row_schema.push_back(col_schema);
+
+  // Schema for local_type_name
+  col_schema.col_index++;
+  col_schema.col_type = BQDataType::kString;
+  row_schema.push_back(col_schema);
+
+  // Schema for minimum_scale
+  col_schema.col_index++;
+  col_schema.col_type = BQDataType::kInt64;
+  row_schema.push_back(col_schema);
+
+  // Schema for maximum_scale
+  col_schema.col_index++;
+  row_schema.push_back(col_schema);
+
+  // Schema for sql_data_type
+  col_schema.col_index++;
+  row_schema.push_back(col_schema);
+
+  // Schema for sql_datetime_sub
+  col_schema.col_index++;
+  row_schema.push_back(col_schema);
+
+  // Schema for num_prec_radix
+  col_schema.col_index++;
+  row_schema.push_back(col_schema);
+
+  // Schema for interval_precision
+  col_schema.col_index++;
+  row_schema.push_back(col_schema);
+}
+
+SQLRETURN SQLGetTypeInfoInternal(SQLHSTMT stmt_handle, SQLSMALLINT data_type) {
+  SQLRETURN rc = SQL_SUCCESS;
+  StatusRecordOr<StatementHandle*> handle_result =
+      ValidateStatementHandle(stmt_handle);
+  if (!handle_result) {
+    TracePrintInternal(opts, handle_result.GetStatusRecord().message);
+    return handle_result.GetCalculatedReturnCode();
+  }
+
+  StatementHandle& handle = *(*handle_result);
+
+  ResultSet& result_set = handle.GetResultSet();
+  if (data_type == SQL_ALL_TYPES) {
+    for (auto [sql_data_type, bq_data_type_info] : kSqlToBqDataTypes) {
+      for (auto [bq_data_type, type_info] : bq_data_type_info) {
+        result_set.rows.push_back(CreateDSRowFromTypeInfo(type_info));
+      }
+    }
+  } else {
+    if (kSqlToBqDataTypes.count(data_type)) {
+      for (auto [bq_data_type, type_info] : kSqlToBqDataTypes.at(data_type)) {
+        result_set.rows.push_back(CreateDSRowFromTypeInfo(type_info));
+      }
+    }
+  }
+
+  if (!result_set.rows.empty()) {
+    CreateTypeInfoRowSchema(result_set);
+    handle.SetStmtState(StmtStates::kStatementExecutedWithRs);
+  } else {
+    handle.SetStmtState(StmtStates::kStatementExecutedWithoutRs);
+  }
+
   return SQL_SUCCESS;
 }
 
