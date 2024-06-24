85915e23b7c3ffc1f446bf451b459967b819096b
Shivam Dabas <shivam.dabas@37848692.corp-partner.google.com>
2024-06-25 00:18:03 +0530

    Resolve windows SDK error

--- google/cloud/odbc/integration_tests/odbc_driver_tests/descriptor_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/descriptor_test.cc
@@ -194,8 +194,11 @@ void CopyDescRec(std::shared_ptr<ODBCHandles> conn, std::string table_name,
       status = SQLGetDescField(conn->ipd, i + 1, SQL_DESC_TYPE, &desc_copy.type,
                                SQL_IS_SMALLINT, NULL);
     }
-    CheckError(status, "SQLGetDescField(SQL_DESC_TYPE)", conn,
-               __USE_DYNAMIC_STACK_SIZE);
+    #ifdef _WIN32
+    CheckError(status, "SQLGetDescField(SQL_DESC_TYPE)", conn, 1); 
+    #else
+    CheckError(status, "SQLGetDescField(SQL_DESC_TYPE)", conn, __USE_DYNAMIC_STACK_SIZE);
+    #endif
     EXPECT_EQ(ToBqFieldType(desc_copy.type), ToBqFieldType(schema[i].type));
   }
 }
