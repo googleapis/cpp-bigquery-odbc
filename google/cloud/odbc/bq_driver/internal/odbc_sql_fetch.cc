a45d98d894b7eef2dffa32c1ace9a0e437fa6884
meilakh <meilakh@google.com>
2024-07-04 16:10:24 +0000

    comments

--- google/cloud/odbc/bq_driver/internal/odbc_sql_fetch.cc
+++ google/cloud/odbc/bq_driver/internal/odbc_sql_fetch.cc
@@ -48,7 +48,9 @@ StatusRecord WriteToApplicationBuffer(DSValue const& ds_val,
 
 StatusRecord WriteDSRow(DSRow const& ds_row, RowSchema const& schema,
                         DescriptorHandle& ard) {
+  std::cout << "Size: " << schema.size() << "\n";
   for (ColumnSchema const& col_schema : schema) {
+    std::cout << "Column ind: " << col_schema.col_index << " : Column: " << col_schema.col_type << "\n";
     int col_index = col_schema.col_index;
     DSValue const& ds_val = ds_row[col_index];
     // Column is not bound.
