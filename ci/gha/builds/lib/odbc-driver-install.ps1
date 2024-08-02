0314d2e1b44669b881c1d01d3728dc3597ba8690
Sachin Purohit <sachinpurohit@google.com>
2024-08-02 10:47:59 +0000

    feat(ci-gha): Installing ODBC driver for integration tests on windows

---
+++ ci/gha/builds/lib/odbc-driver-install.ps1
@@ -0,0 +1,70 @@
+#
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Variable Initialization (Similar to "declare -i" in bash)
+$CI_CLOUDBUILD_BUILDS_LIB_ODBC_DRIVER_INSTALL_SH__ = $null
+if (-not [string]::IsNullOrEmpty($env:CI_CLOUDBUILD_BUILDS_LIB_ODBC_DRIVER_INSTALL_SH__)) {
+    $CI_CLOUDBUILD_BUILDS_LIB_ODBC_DRIVER_INSTALL_SH__ = 0
+}
+
+# Include Guard (Similar to "if ((...)); then return 0; fi" in bash)
+if ($CI_CLOUDBUILD_BUILDS_LIB_ODBC_DRIVER_INSTALL_SH__ -ne $null -and ++$CI_CLOUDBUILD_BUILDS_LIB_ODBC_DRIVER_INSTALL_SH__ -ne 0) {
+    return 0
+}
+
+# Set Environment Variables
+$env:GCS_BUCKET = "bq-dev-tools-testing-drivers"
+$env:ODBC_DRIVER_VERSION = "3.0.5.1011"
+if ([string]::IsNullOrEmpty($env:DRIVER_ARCH)) {
+    throw "DRIVER_ARCH environment variable is not set or empty. Please provide a valid architecture."
+}
+
+if ($env:DRIVER_ARCH -eq 'x64') {
+    $arch = '64'
+} elseif ($env:DRIVER_ARCH -eq 'x86') {
+    $arch = '32'
+} else {
+    Write-Error "Invalid architecture: $env:DRIVER_ARCH"
+    exit 1
+}
+$env:ODBC_DRIVER_MSI_NAME = "SimbaODBCDriverforGoogleBigQuery${arch}_${env:ODBC_DRIVER_VERSION}.msi"
+
+# Download from Google Cloud Storage (gsutil equivalent)
+Write-Output "Downloading $env:ODBC_DRIVER_MSI_NAME from Google Cloud Storage..."
+gsutil -m cp gs://${env:GCS_BUCKET}/odbc-windows/${arch}/${env:ODBC_DRIVER_MSI_NAME} . # Assuming gsutil is installed
+
+# Install MSI (with logging to a file)
+$installerPath = (Resolve-Path $env:ODBC_DRIVER_MSI_NAME).Path
+$logFilePath = "msiexec_install.log"
+
+Write-Output "Installing ODBC driver from $installerPath..."
+Start-Process msiexec.exe -ArgumentList "/i `"$installerPath`" /qn /l*v `"$logFilePath`"" -Wait -NoNewWindow
+
+Write-Output "Installation completed. Log contents:"
+Get-Content $logFilePath | Write-Output
+
+# Download from Google Cloud Storage (gsutil equivalent)
+Write-Output "Downloading service account key from Secret Manager..."
+gcloud secrets versions access latest --secret=service-account-auth-keys --out-file="service_account_auth_keys.json"
+Write-Output "Downloaded service account key"
+
+
+#Write-Output "Downloading system_dsn_setup.reg from Google Cloud Storage..."
+#gsutil -m cp gs://${env:GCS_BUCKET}/odbc-windows/64/system_dsn_setup.reg .
+
+Write-Output "Running DSN creation script from $($PWD.Path)\system_dsn_setup.reg ..."
+# reg import .\system_dsn_setup.reg
+reg import .\ci\gha\builds\lib\system_dsn_setup_${arch}.reg
+Write-Output "DSN creation completed."
---
+++ ci/gha/builds/windows-cmake-integration.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+#
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euo pipefail
+
+source "$(dirname "$0")/../../lib/init.sh"
+source module ci/gha/builds/lib/windows.sh
+source module ci/gha/builds/lib/cmake.sh
+
+export ODBC_TESTS_DSN="SampleDSN"
+
+if [[ -z "${CMAKE_OUT:-}" ]]; then
+  CMAKE_OUT=cmake-out
+fi
+mapfile -t args < <(cmake::common_args "${CMAKE_OUT}")
+mapfile -t vcpkg_args < <(cmake::vcpkg_args)
+mapfile -t ctest_args < <(ctest::common_args)
+if [[ $# -gt 1 ]]; then
+  args+=("-DCMAKE_BUILD_TYPE=${1}")
+  shift
+fi
+if command -v sccache >/dev/null 2>&1; then
+  args+=(
+    # sccache requires specific workarounds with MSVC.
+    -DCMAKE_PROJECT_cpp-bigquery-odbc_INCLUDE="$(dirname "$0")/cmake/windows-sccache.cmake"
+  )
+fi
+# Disable manifest [[1]] generation.  These are known to cause flakes in CI
+# systems [[2]], and we do not need manifests for our purposes.
+#
+# [1]: https://learn.microsoft.com/en-us/windows/win32/sbscs/manifests
+# [2]: https://stackoverflow.com/questions/3775406
+args+=("-DCMAKE_EXE_LINKER_FLAGS=/MANIFEST:NO")
+
+args+=("-DODBC_INTEGRATION_TESTING=ON")
+args+=("-DBQ_DRIVER_INTEGRATION_TESTS=OFF")
+args+=("-DCLIENT_LIBRARY_INTEGRATION_TESTING=OFF")
+args+=("-DODBC_UNIT_TESTING=ON")
+
+io::log_h1 "Starting Build"
+TIMEFORMAT="==> 🕑 CMake configuration done in %R seconds"
+time {
+  # Always run //google/cloud:status_test in case the list of targets has
+  # no unit tests.
+  io::run cmake "${args[@]}" "${vcpkg_args[@]}" -DCMAKE_CXX_STANDARD=20
+}
+
+if command -v sccache >/dev/null 2>&1; then
+  io::log "Current sccache stats"
+  sccache --show-stats
+fi
+
+TIMEFORMAT="==> 🕑 CMake build done in %R seconds"
+time {
+  # Always run //google/cloud:status_test in case the list of targets has
+  # no unit tests.
+  io::run cmake --build "${CMAKE_OUT}"
+}
+
+TIMEFORMAT="==> 🕑 CMake test done in %R seconds"
+time {
+  io::run ctest "${ctest_args[@]}" --test-dir "${CMAKE_OUT}" -LE integration-test
+}
