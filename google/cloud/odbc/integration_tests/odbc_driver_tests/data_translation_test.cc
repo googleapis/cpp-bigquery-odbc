47670fe8a9c1f673c304f459d1a5b1134fadcd5c
Neeraj Dwivedi <neeraj.dwivedi@37848692.corp-partner.google.com>
2024-08-06 10:42:35 +0530

    impl(bq_driver): interval conversion integration test (Simba)

--- google/cloud/odbc/integration_tests/odbc_driver_tests/data_translation_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/data_translation_test.cc
@@ -47,6 +47,15 @@ struct Int64BasicTestStruct {
   SQLRETURN status;
 };
 
+struct IntervalBasicTestStruct {
+  // The target C type SQLGetData will convert SQL type to
+  SQLSMALLINT target_c_type;
+  // The value that should be returned by SQLGetData if it succeeds
+  SQL_INTERVAL_STRUCT interval_value;
+  // The status that should be returned by SQLGetData for this C Type
+  SQLRETURN status;
+};
+
 std::vector<StrBasicTestStruct> const kConversionFromStrTestData{
     {SQL_C_CHAR, "Test String 1", SQL_SUCCESS},
     {SQL_C_FLOAT, "19.1", SQL_SUCCESS},
@@ -114,6 +123,54 @@ std::vector<Int64BasicTestStruct> const kConversionFromInt64TestData{
     {SQL_C_BIT, 1, SQL_SUCCESS},
     {SQL_C_BIT, 2, SQL_ERROR},
 };
+
+StdIntervalRows const kIntervalSampleData{
+    {1, {SQL_IS_YEAR, 1, {.year_month = {3, 0}}}},              // 3 years
+    {2, {SQL_IS_MONTH, 1, {.year_month = {0, 8}}}},             // 8 months
+    {3, {SQL_IS_DAY, 1, {.day_second = {15, 0, 0, 0, 0}}}},     // 15 days
+    {4, {SQL_IS_HOUR, 1, {.day_second = {0, 20, 0, 0, 0}}}},    // 20 hours
+    {5, {SQL_IS_MINUTE, 1, {.day_second = {0, 0, 45, 0, 0}}}},  // 45 minutes
+    {6, {SQL_IS_SECOND, 1, {.day_second = {0, 0, 0, 30, 0}}}},  // 30 seconds
+    {7,
+     {SQL_IS_YEAR_TO_MONTH, 1, {.year_month = {2, 5}}}},  // 2 years, 5 months
+    {8,
+     {SQL_IS_DAY_TO_HOUR,
+      1,
+      {.day_second = {10, 14, 0, 0, 0}}}},  // 10 days, 14 hours
+    {9,
+     {SQL_IS_DAY_TO_MINUTE,
+      1,
+      {.day_second = {5, 0, 30, 0, 0}}}},  // 5 days, 30 minutes
+    {10,
+     {SQL_IS_DAY_TO_SECOND,
+      1,
+      {.day_second = {2, 0, 0, 20,
+                      500}}}},  // 2 days, 20 seconds, 500 milliseconds
+    {11,
+     {SQL_IS_HOUR_TO_MINUTE,
+      1,
+      {.day_second = {0, 9, 45, 0, 0}}}},  // 9 hours, 45 minutes
+    {12,
+     {SQL_IS_HOUR_TO_SECOND,
+      1,
+      {.day_second = {0, 11, 0, 25,
+                      300}}}},  // 11 hours, 25 seconds, 300 milliseconds
+    {13,
+     {SQL_IS_MINUTE_TO_SECOND,
+      1,
+      {.day_second = {0, 0, 50, 10,
+                      100}}}}  // 50 minutes, 10 seconds, 100 milliseconds
+};
+
+std::vector<IntervalBasicTestStruct> const kConversionFromIntervalTestData{
+    {SQL_C_CHAR, {SQL_IS_YEAR, 1, {.year_month = {3, 0}}}, SQL_SUCCESS},
+    {SQL_C_CHAR, {SQL_IS_MONTH, 1, {.year_month = {0, 8}}}, SQL_SUCCESS},
+    {SQL_C_CHAR,
+     {SQL_IS_DAY, 1, {.day_second = {15, 0, 0, 0, 0}}},
+     SQL_SUCCESS},
+    {SQL_C_BINARY,
+     {SQL_IS_HOUR, 1, {.day_second = {0, 20, 0, 0, 0}}},
+     SQL_SUCCESS}};
 
 template <typename TestStruct>
 void TestTranslationsFromArithmetic(std::shared_ptr<ODBCHandles> conn,
@@ -286,6 +343,150 @@ void TestTranslationsFromString(std::shared_ptr<ODBCHandles> conn,
   EXPECT_EQ(row_count, kConversionFromStrTestData.size());
 }
 
+inline std::string formatIntervalString(SQL_INTERVAL_STRUCT interval) {
+  char buffer[50];
+
+  switch (interval.interval_type) {
+    case SQL_IS_YEAR:
+      snprintf(buffer, sizeof(buffer), "%d-0 0 0:0:0",
+               interval.intval.year_month.year);
+      break;
+    case SQL_IS_MONTH:
+      snprintf(buffer, sizeof(buffer), "0-%d 0 0:0:0",
+               interval.intval.year_month.month);
+      break;
+    case SQL_IS_YEAR_TO_MONTH:
+      snprintf(buffer, sizeof(buffer), "%d-%d 0 0:0:0",
+               interval.intval.year_month.year,
+               interval.intval.year_month.month);
+      break;
+    case SQL_IS_DAY:
+      snprintf(buffer, sizeof(buffer), "0-0 %d 0:0:0",
+               interval.intval.day_second.day);
+      break;
+    case SQL_IS_HOUR:
+      snprintf(buffer, sizeof(buffer), "0-0 0 %d:0:0",
+               interval.intval.day_second.hour);
+      break;
+    case SQL_IS_MINUTE:
+      snprintf(buffer, sizeof(buffer), "0-0 0 0:%d:0",
+               interval.intval.day_second.minute);
+      break;
+    case SQL_IS_SECOND:
+      snprintf(buffer, sizeof(buffer), "0-0 0 0:0:%d",
+               interval.intval.day_second.second);
+      break;
+    case SQL_IS_DAY_TO_HOUR:
+      snprintf(buffer, sizeof(buffer), "0-0 %d %d:0:0",
+               interval.intval.day_second.day, interval.intval.day_second.hour);
+      break;
+    case SQL_IS_DAY_TO_MINUTE:
+      snprintf(buffer, sizeof(buffer), "0-0 %d %d:%d:0",
+               interval.intval.day_second.day, interval.intval.day_second.hour,
+               interval.intval.day_second.minute);
+      break;
+    case SQL_IS_DAY_TO_SECOND:
+      snprintf(buffer, sizeof(buffer), "0-0 %d %d:%d:%d",
+               interval.intval.day_second.day, interval.intval.day_second.hour,
+               interval.intval.day_second.minute,
+               interval.intval.day_second.second);
+      break;
+    case SQL_IS_HOUR_TO_MINUTE:
+      snprintf(buffer, sizeof(buffer), "0-0 0 %d:%d:0",
+               interval.intval.day_second.hour,
+               interval.intval.day_second.minute);
+      break;
+    case SQL_IS_HOUR_TO_SECOND:
+      snprintf(buffer, sizeof(buffer), "0-0 0 %d:%d:%d",
+               interval.intval.day_second.hour,
+               interval.intval.day_second.minute,
+               interval.intval.day_second.second);
+      break;
+    case SQL_IS_MINUTE_TO_SECOND:
+      snprintf(buffer, sizeof(buffer), "0-0 0 0:%d:%d",
+               interval.intval.day_second.minute,
+               interval.intval.day_second.second);
+      break;
+    default:
+      snprintf(buffer, sizeof(buffer), "Unknown interval type");
+      break;
+  }
+  return std::string(buffer);
+}
+
+// bool parseIntervalString(char* input_str, SQL_INTERVAL_STRUCT& interval){
+//    int days, hours, minutes, seconds;
+//    if (sscanf(input_str, "0-0 %d %d:%d:%d", &days, &hours, &minutes,
+//    &seconds) != 4) {
+//         std::cerr << "Error parsing input string: " << input_str <<
+//         std::endl; return false;
+//     }
+//     interval.interval_type = SQL_IS_HOUR;  // Set the interval type to
+//     SQL_IS_HOUR interval.interval_sign = 1;            // Positive interval
+
+//     // Populate the day_second struct with the parsed values
+//     interval.intval.day_second.day = days;
+//     interval.intval.day_second.hour = hours;
+//     interval.intval.day_second.minute = minutes;
+//     interval.intval.day_second.second = seconds;
+//     interval.intval.day_second.fraction = 0;  // Assume no fraction in input
+//     return true;
+// }
+
+void TestTranslationFromInterval(std::shared_ptr<ODBCHandles> conn,
+                                 std::string query) {
+  SQLRETURN status;
+  char read_stmt[kBufferLength];
+  SQLCHAR data[kBufferLength];
+  SQLLEN strlen_or_ind;
+
+  StrToChar(read_stmt, query.c_str());
+  std::cout << "fetch Query " << query << std::endl;
+
+  int row_count = 0;
+  status = SQLPrepare(conn->hstmt, (SQLCHAR*)read_stmt, SQL_NTS);
+  CheckError(status, "SQLPrepare", conn);
+
+  status = SQLExecute(conn->hstmt);
+  CheckError(status, "SQLExecute", conn);
+
+  for (auto const& expected : kConversionFromIntervalTestData) {
+    status = SQLBindCol(conn->hstmt, 1, expected.target_c_type, data,
+                        kBufferLength, &strlen_or_ind);
+    CheckError(status, "SQLBindCol", conn);
+
+    status = SQLFetch(conn->hstmt);
+    if (status == SQL_NO_DATA) {
+      break;
+    }
+
+    if (SQL_SUCCEEDED(status)) {
+      CheckError(status, "SQLFetch", conn);
+      break;
+    }
+    switch (expected.target_c_type) {
+      case SQL_C_CHAR: {
+        std::string returned_val = reinterpret_cast<char*>(data);
+        std::string expected_val =
+            formatIntervalString(expected.interval_value);
+        EXPECT_EQ(expected_val, returned_val);
+        break;
+      }
+      case SQL_C_WCHAR: {
+        // To be implemented
+        std::string returned_val = reinterpret_cast<char*>(data);
+      }
+      case SQL_C_BINARY: {
+        // SImba data not converting to SQL interval struut.
+      }
+
+      default:
+        break;
+    }
+    ++row_count;
+  }
+}
+
 // This test should follow translations according to
 // https://learn.microsoft.com/en-us/sql/odbc/reference/appendixes/sql-to-c-character?view=sql-server-ver16
 TEST(DataTranslationTest, From_SQL_CHAR_to_all) {
@@ -392,6 +593,34 @@ TEST(DataTranslationTest, From_INT64_to_all) {
   EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
 }
 
+TEST(DataTranslationTest, From_Interval_to_all) {
+  auto const table_name =
+      kDatasetWithTablePrefix + "ODBC_DATA_TRANSLATION_SQL_INTERVAL";
+  Table table(table_name);
+  auto conn = std::make_shared<ODBCHandles>();
+
+  // Create Table
+  EXPECT_EQ(Connect(kDefaultConnectionString, conn), SQL_SUCCESS);
+  table.Create(conn, "(index INT64, IntervalField INTERVAL)");
+  EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
+
+  // Insert data to read
+  EXPECT_EQ(Connect(kDefaultConnectionString, conn), SQL_SUCCESS);
+  table.InsertIntervalData(conn, kIntervalSampleData);
+  EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
+
+  // Read data
+  EXPECT_EQ(Connect(kDefaultConnectionString, conn), SQL_SUCCESS);
+  std::string qry =
+      "SELECT IntervalField FROM " + table_name + " ORDER BY index";
+  TestTranslationFromInterval(conn, qry);
+  EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
+
+  // drop table
+  EXPECT_EQ(Connect(kDefaultConnectionString, conn), SQL_SUCCESS);
+  table.DropWithPrepare(conn);
+  EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
+}
 #endif  // BQ_DRIVER_INTEGRATION_TESTS
 
 }  // namespace google::cloud::odbc_tests
