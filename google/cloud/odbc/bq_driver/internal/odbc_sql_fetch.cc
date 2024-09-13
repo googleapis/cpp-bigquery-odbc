a0f93612b0f2926af5a02241c2474d22498fce5e
Neeraj Dwivedi <neeraj.dwivedi@37848692.corp-partner.google.com>
2024-09-13 15:10:38 +0530

    impl(bq_driver): Add support for interval conversions

--- google/cloud/odbc/bq_driver/internal/odbc_sql_fetch.cc
+++ google/cloud/odbc/bq_driver/internal/odbc_sql_fetch.cc
@@ -55,6 +55,8 @@ StatusRecord WriteToApplicationBuffer(DSValue const& ds_val,
       return ConvertFromDateDSValue(ds_val, data);
     case BQDataType::kTime:
       return ConvertFromTimeDSValue(ds_val, data);
+    case BQDataType::kInterval:
+      return ConvertFromIntervalDSValue(ds_val, data);
   }
   return {SQLStates::k_HYC00(), "Data type not supported"};
 }
