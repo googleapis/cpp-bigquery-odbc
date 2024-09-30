6333e12250c90644c3c46ac024c63e3dba82f181
KanchanShu <kanchan.shukla@37848692.corp-partner.google.com>
2024-09-30 20:18:17 +0530

    modified test

--- google/cloud/odbc/integration_tests/odbc_driver_tests/connection_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/connection_test.cc
@@ -692,7 +692,7 @@ TEST(ConnectionTest, SQLSetConnectAttrW_UpdateString) {
   CheckError(status, "SQLGetConnectAttr", conn);
   std::string str_out =
       ConvertSQLWCHARToString(output, length / sizeof(SQLWCHAR));
-  EXPECT_STREQ(expected.data, str_out.data());
+  EXPECT_STREQ(expected.data(), str_out.data());
   EXPECT_EQ(expected.size(), length / sizeof(SQLWCHAR));
 
   EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
