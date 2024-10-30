db751cda4852b01077d610aeedaeee7d5e89b83e
Khushi Kathuria <khushi.kathuria@37848692.corp-partner.google.com>
2024-10-30 17:38:37 +0530

    test

--- google/cloud/odbc/bq_driver/odbc_commons.cc
+++ google/cloud/odbc/bq_driver/odbc_commons.cc
@@ -143,9 +143,19 @@ bool ConfigDSNInternal(HWND hwnd_parent, WORD f_request, LPCSTR lpsz_driver,
   std::string catalog;
   std::string dataset_name;
 
+  
   DriverForm form;
   switch (f_request) {
     case ODBC_ADD_DSN: {
+      if(hwnd_parent==NULL){
+        Section section = {{"Email", email},
+                         {"KeyFilePath", key_file_path},
+                         {"OAuthMechanism", oAuth_mechanism},
+                         {"Catalog", catalog},
+                         {"Dataset", dataset_name}};
+      AddDSNToRegistry(dsn_value, lpsz_driver, section);
+      return true;
+      }
       form.Show();
       form.GetHwnd();
       MSG msg = {};
@@ -169,6 +179,15 @@ bool ConfigDSNInternal(HWND hwnd_parent, WORD f_request, LPCSTR lpsz_driver,
       return TRUE;
     }
     case ODBC_CONFIG_DSN: {
+      if(hwnd_parent==NULL){
+         Section section2 = {{"Email", email},
+                          {"KeyFilePath", key_file_path},
+                          {"OAuthMechanism", oAuth_mechanism},
+                          {"Catalog", catalog},
+                          {"Dataset", dataset_name}};
+      EditDSNInRegistry(dsn_value, section2);
+      return true;
+      }
       std::string registry_key = GetPathToOdbcIni() + "\\" + dsn_value;
       auto res = GetSectionWin(registry_key);
       auto section = res.GetValue();
--- google/cloud/odbc/bq_driver/odbc_commons_test.cc
+++ google/cloud/odbc/bq_driver/odbc_commons_test.cc
@@ -20,14 +20,7 @@
 #include "google/cloud/odbc/internal/odbc_includes.h"
 #include "google/cloud/odbc/testing/bq_driver_utils/handles.h"
 #include <gtest/gtest.h>
-
-#ifdef _WIN32
-#include "google/cloud/odbc/bq_driver/internal/driver_form.h"
-#endif  // _WIN32
-
-#ifdef _WIN32
-#include "google/cloud/odbc/bq_driver/internal/driver_form.h"
-#endif  // _WIN32
+#include "google/cloud/odbc/bq_driver/internal/utils.h"
 
 namespace google::cloud::odbc_bq_driver {
 
@@ -40,12 +33,9 @@ using google::cloud::odbc_testing_bq_driver_utils::CreateConnectionHandle;
 using google::cloud::odbc_testing_bq_driver_utils::CreateStatementHandle;
 
 #ifdef _WIN32
-using google::cloud::odbc_bq_driver_internal::DriverForm;
-#endif  // _WIN32
-
-#ifdef _WIN32
-using google::cloud::odbc_bq_driver_internal::DriverForm;
-#endif  // _WIN32
+using google::cloud::odbc_bq_driver_internal::GetSectionWin;
+using google::cloud::odbc_bq_driver_internal::Section;
+#endif
 
 TEST(SQLFreeHandleInternal, InvalidType) {
   int val = 10;
@@ -182,6 +172,31 @@ TEST(ConfigDSNInternal, NullRequest) {
       ConfigDSNInternal(hwndParent, fRequest, lpszDriver, lpszAttributes);
   EXPECT_EQ(status, false);
 }
+
+
+TEST(ConfigDSNInternal, NullhandleSucess) {
+  HWND hwndParent = NULL;
+  WORD fRequest = ODBC_ADD_DSN;
+  LPCSTR lpszDriver = "ODBC Driver For Google Bigquery";
+  LPCSTR lpszAttributes =
+      "DSN=Personnel Data\0Email=Smith.Sesame@gmail.com\0Dataset=Personnel\0\0";
+  auto result =
+      ConfigDSNInternal(hwndParent, fRequest, lpszDriver, lpszAttributes);
+  EXPECT_EQ(result, true);
+
+  auto status = GetSectionWin("SOFTWARE\\ODBC\\ODBC.INI\\Personnel Data");
+  std::shared_ptr<Section> section2 = status.GetValue();
+  ASSERT_TRUE(section2);
+
+  EXPECT_EQ(section2->at("Email"), "Smith.Sesame@gmail.com");
+  EXPECT_EQ(section2->at("Dataset"), "Personnel");
+
+ result =
+      ConfigDSNInternal(hwndParent,ODBC_REMOVE_DSN, lpszDriver, lpszAttributes);
+  EXPECT_EQ(result, true);
+
+}
+
 #endif  // _WIN32
 
 }  // namespace google::cloud::odbc_bq_driver
