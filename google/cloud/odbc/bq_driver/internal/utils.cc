59b3009eb519d85cabb44bebf6dace57d16d3c74
Neeraj Dwivedi <neeraj.dwivedi@37848692.corp-partner.google.com>
2024-11-05 19:20:08 +0530

    impl(bq_driver): Add Code Improved Changes

--- google/cloud/odbc/bq_driver/internal/utils.cc
+++ google/cloud/odbc/bq_driver/internal/utils.cc
@@ -222,6 +222,29 @@ StatusRecordOr<Section> ParseConnectionString(std::string& str) {
   return section;
 }
 
+std::vector<std::string> ExtractKeys(std::string& str, bool is_parameterized) {
+  std::smatch match;
+  std::vector<std::string> keywords;
+  std::regex regex_pattern;
+
+  if (is_parameterized) {
+    regex_pattern =
+        std::regex(R"(\b([A-Za-z0-9_-]+)(?=:))");  // Pattern to capture
+                                                   // keywords before ":"
+  } else {
+    regex_pattern =
+        std::regex(R"(\b([A-Za-z0-9_-]+)(?==))");  // Pattern to capture
+                                                   // keywords before "="
+  }
+
+  std::string::const_iterator search_start(str.cbegin());
+  while (std::regex_search(search_start, str.cend(), match, regex_pattern)) {
+    keywords.push_back(match[1]);
+    search_start = match.suffix().first;
+  }
+  return keywords;
+}
+
 std::string GetPathToOdbcIni() {
 #ifdef _WIN32
   // 64-bit
