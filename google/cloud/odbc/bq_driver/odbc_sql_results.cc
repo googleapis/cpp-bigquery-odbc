c77d2405ea993efe96893c1dd25df90012b65a16
Khushi Kathuria <khushi.kathuria@37848692.corp-partner.google.com>
2025-01-10 19:19:08 +0530

    test

--- google/cloud/odbc/bq_driver/odbc_sql_results.cc
+++ google/cloud/odbc/bq_driver/odbc_sql_results.cc
@@ -452,6 +452,8 @@ SQLRETURN SQLRowCountInternal(SQLHSTMT statement_handle, SQLLEN* row_count) {
       status_record = {SQLStates::k_HY001(), "Statement is not prepared"};
       break;
     case StmtStates::kStatementAsyncExecute:
+      status_record = {SQLStates::k_HY010(), "Function sequence error"};
+      break;
     case StmtStates::kStatementAsyncPrepare:
       status_record = {SQLStates::k_HY010(), "Function sequence error"};
       break;
