bacba9d658e6dfb3ac2a7a96679f1948ea8f9b17
KanchanShu <kanchan.shukla@37848692.corp-partner.google.com>
2025-02-14 17:13:22 +0530

    fix test

--- google/cloud/odbc/testing/odbc_utils/commons.cc
+++ google/cloud/odbc/testing/odbc_utils/commons.cc
@@ -1646,6 +1646,7 @@ std::string ConvertSQLWCHARToString(SQLWCHAR* in_str, SQLINTEGER in_str_len) {
   for (SQLINTEGER i = 0; i < in_str_len; ++i) {
     stmt_txt_wstr.push_back(static_cast<wchar_t>(in_str[i]));
   }
+  stmt_txt_wstr.push_back(L'\0');
   return Utf16ToUtf8(stmt_txt_wstr);
 }
 
