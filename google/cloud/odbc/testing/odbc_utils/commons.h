7fd8cfd129b528b3bbc7bb0562c78beaeecf6790
KanchanShu <kanchan.shukla@37848692.corp-partner.google.com>
2025-02-14 17:43:32 +0530

    test

--- google/cloud/odbc/testing/odbc_utils/commons.h
+++ google/cloud/odbc/testing/odbc_utils/commons.h
@@ -622,7 +622,7 @@ void BindColManually(std::shared_ptr<ODBCHandles> conn,
 void BindStdColumns(std::shared_ptr<ODBCHandles> conn,
                     TestingDataBuffer* columns);
 
-std::string Utf16ToUtf8(std::wstring const& utf_16_str);
+std::string Utf16ToUtf8(std::wstring utf_16_str);
 
 std::wstring Utf8ToUtf16(std::string const& utf_8_str);
 
