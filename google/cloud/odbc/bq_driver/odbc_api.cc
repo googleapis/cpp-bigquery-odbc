68b58a86ae633325da07715b48e88c76407454a1
KanchanShu <kanchan.shukla@37848692.corp-partner.google.com>
2025-02-19 13:50:08 +0530

    test

--- google/cloud/odbc/bq_driver/odbc_api.cc
+++ google/cloud/odbc/bq_driver/odbc_api.cc
@@ -867,7 +867,7 @@ SQLRETURN SQL_API SQLGetInfoW(SQLHDBC connectionHandle, SQLUSMALLINT infoType,
   rc = ::google::cloud::odbc_bq_driver::SQLGetInfoInternal(
       connectionHandle, infoType, info_val_buffer, infoValueBufferLen,
       &info_val_buffer_len);
-std::cout<<"infoValue "<<(char*)infoValue<<std::endl;
+std::memset(infoValue, '\0', infoValueBufferLen);
   // Handle Unicode conversion of output parameters.
   if (SQL_SUCCEEDED(rc) && info_val_buffer_len > 0) {
     if (IsInfoTypeString(infoType)) {
