7249fc537979b4a5e17e76ebbae3a4fe188e10e4
amrendraverma14 <amrendrakumar.verma@37848692.corp-partner.google.com>
2025-02-21 13:12:07 +0000

    fix: resolving review comments and check failures

--- google/cloud/odbc/bq_driver/internal/odbc_internal_commons.cc
+++ google/cloud/odbc/bq_driver/internal/odbc_internal_commons.cc
@@ -408,7 +408,7 @@ StatusRecordOr<ResultSet> ProcessResultSetRows(
         DSValue row_val;
         switch (col_type) {
           case BQDataType::kNumeric:
-          case BQDataType::kBigNumeric: 
+          case BQDataType::kBigNumeric:
           case BQDataType::kFloat64: {
             SQLDOUBLE d_data = std::stod(data);
             ArithmeticToDSValue<SQLDOUBLE>(d_data, row_val);
