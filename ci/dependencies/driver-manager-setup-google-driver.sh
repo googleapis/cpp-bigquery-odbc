97d0a7b16ff593eefec0ad4508631e711dfcff73
shivamd-gpartner <shivam.dabas@37848692.corp-partner.google.com>
2025-02-26 11:59:20 +0530

    Add session datasource

--- ci/dependencies/driver-manager-setup-google-driver.sh
+++ ci/dependencies/driver-manager-setup-google-driver.sh
@@ -48,6 +48,7 @@ fi
 echo 'Configuring Environment Variables For ODBC Driver...'
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:/usr/local/lib/
 export ODBCINI=/opt/odbc-driver/odbc.ini
+export CPP_BIGQUERY_ODBC_TEST_SERVICE_ACCOUNT_AUTH_KEY=/opt/odbc-driver/connection/key.json
 
 cd "$CPP_GOOGLE_BIGQUERY_ODBC_DRIVER_MANAGER_SETUP_CURR_DIR"
 
