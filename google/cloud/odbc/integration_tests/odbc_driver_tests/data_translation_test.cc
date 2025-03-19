ff02ab483d02b6da8032de66db9b775c258098f2
amrendraverma14 <amrendrakumar.verma@37848692.corp-partner.google.com>
2025-03-19 16:51:24 +0000

    review comments -5

--- google/cloud/odbc/integration_tests/odbc_driver_tests/data_translation_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/data_translation_test.cc
@@ -106,10 +106,13 @@ std::vector<NumericBasicTestStruct> const kConversionFromNumericTestData{
     {SQL_C_NUMERIC, "123.78", SQL_SUCCESS,
      "123.78"},  // returns 123- scale value is always 0 for existing Driver
     {SQL_C_NUMERIC, "999.78354", SQL_SUCCESS,
-     "999.78354"},  // Existing driver returns 999 - scale value is always 0 for Existing Driver
+     "999.78354"},  // Existing driver returns 999 - scale value is always 0 for
+                    // Existing Driver
     {SQL_C_NUMERIC, "123.7835", SQL_SUCCESS,
-     "123.7835"},  // Existing Driver returns 123 - scale value is always 0 for Existing Driver
-    {SQL_C_CHAR, "-123", SQL_SUCCESS},  // in Existing Driver it returns "-123.000000000"
+     "123.7835"},  // Existing Driver returns 123 - scale value is always 0 for
+                   // Existing Driver
+    {SQL_C_CHAR, "-123",
+     SQL_SUCCESS},  // in Existing Driver it returns "-123.000000000"
     {SQL_C_CHAR, "123.222",
      SQL_SUCCESS},  // in Existing Driver it returns "123.222000000"
     {SQL_C_CHAR, "-123.222", SQL_SUCCESS},
@@ -181,11 +184,14 @@ std::vector<
     {SQL_C_DOUBLE, "9.9999999999999999999999999999999999999E+28", SQL_SUCCESS},
 #ifdef BQ_DRIVER_INTEGRATION_TESTS
     {SQL_C_CHAR, "-123",
-     SQL_SUCCESS},  // Existing Driver returns -123.00000000000000000000000000000000000000
+     SQL_SUCCESS},  // Existing Driver returns
+                    // -123.00000000000000000000000000000000000000
     {SQL_C_CHAR, "123.222",
-     SQL_SUCCESS},  // Existing Driver returns 123.22200000000000000000000000000000000000
+     SQL_SUCCESS},  // Existing Driver returns
+                    // 123.22200000000000000000000000000000000000
     {SQL_C_CHAR, "-123.222",
-     SQL_SUCCESS},  // Existing Driver returns 123.22200000000000000000000000000000000000
+     SQL_SUCCESS},  // Existing Driver returns
+                    // 123.22200000000000000000000000000000000000
     {SQL_C_CHAR, "99999999999999999999999999999.999999999",
      SQL_SUCCESS},  //"99999999999999999999999999999.99999999900000000000000000000000000000"
     {SQL_C_CHAR, "123123123123123123123.222", SQL_SUCCESS},
@@ -672,9 +678,9 @@ TEST(DataTranslationTest, From_BIGNUMERIC_All) {
 }
 
 // This test case is to make sure if the column type is NUMERIC or BIGNUMERIC
-//Then the column must not accept any value beyond its legitimate range 
-//For range of data types refer : 
-//https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types#numeric_types
+// Then the column must not accept any value beyond its legitimate range
+// For range of data types refer :
+// https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types#numeric_types
 
 TEST(DataTranslationTest, BIGNUMERIC_Invalid_Data_Test) {
   std::string data_big_numeric = "BIGNUMERIC";
