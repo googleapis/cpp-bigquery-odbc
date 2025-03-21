5c815a73df52aad63234dcde3afc4123ef3af768
Suresh Gunji <suresh.gunji@37848692.corp-partner.google.com>
2025-03-21 11:37:37 +0530

    impl(bq_driver): fix issue with SQLGetData

--- google/cloud/odbc/bq_driver/internal/odbc_query.cc
+++ google/cloud/odbc/bq_driver/internal/odbc_query.cc
@@ -79,6 +79,9 @@ StatusRecord GetColumnData(DSValue const& ds_val, BQDataType bq_data_type,
     case BQDataType::kBytes:
       status_record = ConvertFromBytesDSValue(ds_val, data);
       break;
+    case BQDataType::kBool:
+      status_record = ConvertFromBooleanDSValue(ds_val, data);
+      break;
     default:
       status_record = {SQLStates::k_HYC00(), "Data type not supported"};
   }
