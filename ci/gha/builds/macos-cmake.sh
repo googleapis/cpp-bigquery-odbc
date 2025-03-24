223b4e86624a987b2d96ab6704535782f1f10072
Shivam Dabas <shivam.dabas@37848692.corp-partner.google.com>
2025-03-24 19:50:14 +0530

    fix the files

--- ci/gha/builds/macos-cmake.sh
+++ ci/gha/builds/macos-cmake.sh
@@ -19,54 +19,47 @@ set -euo pipefail
 source "$(dirname "$0")/../../lib/init.sh"
 source module ci/gha/builds/lib/macos.sh
 source module ci/gha/builds/lib/cmake.sh
-ls -l
-pwd
-cp ci/gha/builds/lib/odbc_osx.ini /Users/runner/work/connection/odbc-driver/odbc.ini
-cp ci/gha/builds/lib/odbcinst_osx.ini /Users/runner/work/connection/odbc-driver/odbcinst.ini
-export ODBCINI=/Users/runner/work/connection/odbc-driver/odbc.ini
-export ODBCINSTINI=/Users/runner/work/connection/odbc-driver/odbcinst.ini
-export ODBC_TESTS_DSN="SampleDSNGoogleDriver"
-echo $ODBCINI
-export CPP_BIGQUERY_ODBC_TEST_SERVICE_ACCOUNT_AUTH_KEY=/Users/runner/work/connection/key.json
-echo "prinintg files heere========="
-echo $CPP_BIGQUERY_ODBC_TEST_SERVICE_ACCOUNT_AUTH_KEY
-echo "prinintg files end========="
-pwd
-mapfile -t args < <(cmake::common_args)
-args+=(
-  -DODBC_UNIT_TESTING=OFF
-  -DODBC_INTEGRATION_TESTING=ON
-  -DBQ_DRIVER_INTEGRATION_TESTS=ON
-  -DCLIENT_LIBRARY_INTEGRATION_TESTING=OFF
-  -DBUILD_SHARED_LIBS=ON
-  -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-  -DCMAKE_CXX_FLAGS="-I$(brew --prefix libiodbc)/include"
-  -DCMAKE_CXX_STANDARD=17
-)
 
-mapfile -t vcpkg_args < <(cmake::vcpkg_args)
-mapfile -t ctest_args < <(ctest::common_args)
+  cp ci/gha/builds/lib/odbc_osx.ini /Users/runner/work/connection/odbc-driver/odbc.ini
+  cp ci/gha/builds/lib/odbcinst_osx.ini /Users/runner/work/connection/odbc-driver/odbcinst.ini
+  export ODBCINI=/Users/runner/work/connection/odbc-driver/odbc.ini
+  export ODBCINSTINI=/Users/runner/work/connection/odbc-driver/odbcinst.ini
+  export ODBC_TESTS_DSN="SampleDSNGoogleDriver"
+  export CPP_BIGQUERY_ODBC_TEST_SERVICE_ACCOUNT_AUTH_KEY=/Users/runner/work/connection/key.json
 
-io::log_h1 "Starting Build"
-TIMEFORMAT="==> 🕑 CMake configuration done in %R seconds"
-time {
-  io::run cmake "${args[@]}" "${vcpkg_args[@]}"
-}
+  mapfile -t args < <(cmake::common_args)
+  args+=(
+    -DODBC_UNIT_TESTING=OFF
+    -DODBC_INTEGRATION_TESTING=ON
+    -DBQ_DRIVER_INTEGRATION_TESTS=ON
+    -DCLIENT_LIBRARY_INTEGRATION_TESTING=OFF
+    -DBUILD_SHARED_LIBS=ON
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+    -DCMAKE_CXX_FLAGS="-I$(brew --prefix libiodbc)/include"
+    -DCMAKE_CXX_STANDARD=17
+  )
 
-TIMEFORMAT="==> 🕑 CMake build done in %R seconds"
-time {
-  io::run cmake --build cmake-out
-}
-echo "CMAKE DONE first"
-ls -l
-ls -l cmake-out
-echo "CMAKE DONE second"
-ls -l cmake-out/google/cloud/odbc
-TIMEFORMAT="==> 🕑 CMake test done in %R seconds"
-time { 
-  io::run ctest "${ctest_args[@]}" --test-dir cmake-out -LE integration-test
-}
-echo "CMAKE DONE HERE"
-cd cmake-out
-ls -l
+  mapfile -t vcpkg_args < <(cmake::vcpkg_args)
+  mapfile -t ctest_args < <(ctest::common_args)
+
+  io::log_h1 "Starting Build"
+  TIMEFORMAT="==> 🕑 CMake configuration done in %R seconds"
+  time {
+    io::run cmake "${args[@]}" "${vcpkg_args[@]}"
+  }
+
+  echo "matreix OS======end"
+  echo $MATRIX_OS
+
+  TIMEFORMAT="==> 🕑 CMake build done in %R seconds"
+  time {
+    io::run cmake --build cmake-out
+  }
+
+if [[ "$MATRIX_OS" == "macos-14" ]]; then
+  TIMEFORMAT="==> 🕑 CMake test done in %R seconds"
+  time { 
+    io::run ctest "${ctest_args[@]}" --test-dir cmake-out -LE integration-test
+  }
+fi
 
