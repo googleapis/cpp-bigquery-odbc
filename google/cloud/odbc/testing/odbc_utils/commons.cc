c409bb63ad9d0442f6cbc0b9be984d25d9946e19
Sachin Purohit <sachinpurohit@google.com>
2025-04-02 11:57:40 +0000

    fix: removing unnecessary SQLWCHAR handling

--- google/cloud/odbc/testing/odbc_utils/commons.cc
+++ google/cloud/odbc/testing/odbc_utils/commons.cc
@@ -1643,9 +1643,6 @@ std::string ConvertSQLWCHARToString(SQLWCHAR* in_str, SQLINTEGER in_str_len) {
   std::wstring wstr(reinterpret_cast<wchar_t const*>(in_str));
   if (in_str_len == SQL_NTS || in_str_len == NULL) {
     in_str_len = wstr.size();
-    if (sizeof(SQLWCHAR) == 2) {
-      in_str_len = in_str_len * sizeof(SQLWCHAR);
-    }
   }
   stmt_txt_wstr.reserve(in_str_len);
   for (SQLINTEGER i = 0; i < in_str_len; ++i) {
