93463aad4f2add1786642b233d95ed213742760e
Anshu6250 <anshu@37848692.corp-partner.google.com>
2025-04-02 12:24:47 +0530

    added scrolling feature

--- google/cloud/odbc/bq_driver/internal/utils.cc
+++ google/cloud/odbc/bq_driver/internal/utils.cc
@@ -197,7 +197,7 @@ HWND CreateScrollableEditBox(HWND parent, int x, int y, int width, int height,
                              int id) {
   return CreateWindowEx(0, "EDIT", "",
                         WS_VISIBLE | WS_CHILD | WS_BORDER | ES_LEFT |
-                            ES_MULTILINE | ES_AUTOVSCROLL | ES_WANTRETURN,
+                            ES_MULTILINE | ES_AUTOVSCROLL | ES_WANTRETURN | WS_VSCROLL,
                         x, y, width, height, parent, (HMENU)id,
                         GetModuleHandle(NULL), NULL);
 }
