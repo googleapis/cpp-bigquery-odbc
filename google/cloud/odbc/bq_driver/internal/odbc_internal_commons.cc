ac5b1d3a6496c3cc0a1b13479569d251ce00e2af
Sachin Purohit <sachinpurohit@google.com>
2025-04-09 09:37:04 +0000

    minor(bq_driver): simplifying the logic of handling empty values

--- google/cloud/odbc/bq_driver/internal/odbc_internal_commons.cc
+++ google/cloud/odbc/bq_driver/internal/odbc_internal_commons.cc
@@ -465,19 +465,6 @@ StatusRecordOr<ResultSet> ProcessResultSetRows(
       std::string data = col.value;
       if (col.is_null) {
         rs_row.emplace_back(kNullValue);
-      } else if (data.empty()) {
-        switch (col_type) {
-          case BQDataType::kString:
-          case BQDataType::kBytes: {
-            DSValue empty_value;
-            StringToDSValue("", empty_value);
-            rs_row.emplace_back(empty_value);
-            break;
-          }
-          default:
-            return StatusRecord{SQLStates::k_HY000(),
-                                "Invalid or unsupported col BQ data type"};
-        }
       } else if (!data.empty()) {
         DSValue row_val;
         switch (col_type) {
@@ -563,6 +550,10 @@ StatusRecordOr<ResultSet> ProcessResultSetRows(
           }
         }
         rs_row.emplace_back(row_val);
+      } else {
+        DSValue empty_value;
+        StringToDSValue("", empty_value);
+        rs_row.emplace_back(empty_value);
       }
       i++;
     }
