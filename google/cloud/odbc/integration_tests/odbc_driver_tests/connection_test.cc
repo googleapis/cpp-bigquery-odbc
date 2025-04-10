dd37d2e5262cf9d68ff06555611c872295511e7b
root <root@DEL-909NQR3.localdomain>
2025-04-10 12:29:54 +0530

    test

--- google/cloud/odbc/integration_tests/odbc_driver_tests/connection_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/connection_test.cc
@@ -1060,6 +1060,54 @@ TEST(ConnectionTest, SQLBrowseConnect_WithDriver) {
   EXPECT_EQ(out_conn_str_len, expected_out_conn_str.size());
 }
 
+TEST(ConnectionTest, SQLBrowseConnect_WithDriver_NULL) {
+  auto conn = std::make_shared<ODBCHandles>();
+  std::string conn_str;
+
+  SQLCHAR in_conn_str[kBufferLength];
+  SQLSMALLINT out_conn_str_len;
+  SQLCHAR out_conn_str[kBufferLength] = {0};
+
+  StrToChar((char*)in_conn_str, conn_str);
+  SetAttributes(conn, 30);
+
+  auto status = SQLBrowseConnect(conn->hdbc, (SQLCHAR*)in_conn_str,
+                                 sizeof(in_conn_str), (SQLCHAR*)out_conn_str,
+                                 sizeof(out_conn_str), &out_conn_str_len);
+
+  EXPECT_EQ(status, SQL_NEED_DATA);
+
+std::string res_out_conn_str(reinterpret_cast<char const*>(out_conn_str));
+    
+  std::cout << "[DEBUG] Output connection string: " << res_out_conn_str << std::endl;
+  std::cout << "[DEBUG] Output string actual size: " << res_out_conn_str.size() << std::endl;
+
+  if (kIsBqDriver) {
+    std::cout << "[DEBUG] kIsBqDriver is true" << std::endl;
+    EXPECT_GE(out_conn_str_len, res_out_conn_str.size());
+  } else {
+    std::cout << "[DEBUG] kIsBqDriver is false" << std::endl;
+    EXPECT_GT(out_conn_str_len, res_out_conn_str.size());
+  }
+
+  // Print final connection string
+
+#ifndef BQ_DRIVER_INTEGRATION_TESTS
+#ifndef _WIN32
+  std::cout << "[DEBUG] Linux non-BQ_DRIVER_INTEGRATION_TESTS build detected." << std::endl;
+  EXPECT_TRUE(res_out_conn_str.empty());
+#else
+  std::cout << "[DEBUG] Windows non-BQ_DRIVER_INTEGRATION_TESTS build detected." << std::endl;
+  EXPECT_THAT(res_out_conn_str,
+              HasSubstr("Catalog:Catalog=?;OAuthMechanism:OAuthMechanism=?"));
+#endif  // _WIN32
+#else
+  std::cout << "[DEBUG] BQ_DRIVER_INTEGRATION_TESTS build detected." << std::endl;
+  EXPECT_THAT(res_out_conn_str,
+              HasSubstr("Catalog:Catalog=?;OAuthMechanism:OAuthMechanism=?"));
+#endif  // BQ_DRIVER_INTEGRATION_TESTS
+}
+
 TEST(ConnectionTest, SQLBrowseConnect_SQL_NEED_DATA) {
   auto conn = std::make_shared<ODBCHandles>();
   std::string const driver_name = GetDriverName();
