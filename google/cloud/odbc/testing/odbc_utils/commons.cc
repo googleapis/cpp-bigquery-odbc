00f48742a16bca7593e10424eb97207a03aa4a90
KanchanShu <Kanchan.Shukla@37848692.corp-partner.google.com>
2025-05-14 10:36:44 +0000

    test

--- google/cloud/odbc/testing/odbc_utils/commons.cc
+++ google/cloud/odbc/testing/odbc_utils/commons.cc
@@ -1544,6 +1544,7 @@ std::string Utf16ToUtf8(std::wstring const& utf_16_str) {
   }
   return utf8Str;
 #else
+std::cout<<"here in common\n";
   iconv_t cd = iconv_open("UTF-8", kFromCode.c_str());
   int errorno = -1;
   int* errorptr = &errorno;
