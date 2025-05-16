26dd12e9471d74a7477e428ef456cc9795a017ce
Neeraj Dwivedii <neeraj.dwivedi@37848692.corp-partner.google.com>
2025-05-16 08:30:55 +0000

    minor fix

--- google/cloud/odbc/bq_driver/internal/data_translation_inv.h
+++ google/cloud/odbc/bq_driver/internal/data_translation_inv.h
@@ -79,7 +79,9 @@ StatusRecordOr<std::string> ConvertFromArithmeticValue(SrcType src_val,
       StatusRecord check_status =
           CheckLimitsArithmetic<SrcType, SQLSMALLINT>(src_val);
       if (check_status.sql_state != SQLStates::k_22003()) {
-        auto dest_val = static_cast<SQLSMALLINT>(src_val);
+        // clang-tidy suggested cast to unsigned char, but it broke conversion;
+        // added + to promote src_val to int
+        auto dest_val = static_cast<SQLSMALLINT>(+src_val);
         return std::to_string(dest_val);
       }
       return check_status;
@@ -98,7 +100,9 @@ StatusRecordOr<std::string> ConvertFromArithmeticValue(SrcType src_val,
       StatusRecord check_status =
           CheckLimitsArithmetic<SrcType, SQLINTEGER>(src_val);
       if (check_status.sql_state != SQLStates::k_22003()) {
-        auto dest_val = static_cast<SQLINTEGER>(src_val);
+        // clang-tidy suggested cast to unsigned char, but it broke conversion;
+        // added + to promote src_val to int
+        auto dest_val = static_cast<SQLINTEGER>(+src_val);
         return std::to_string(dest_val);
       }
       return check_status;
