4d811c6d7e2beea0a83bbd725b6dea35b7ddaf5c
sgunji25 <suresh.gunji@cloudsufi.com.test-google-a.com>
2025-05-16 17:21:12 +0000

    imppl(bq_driver): fix issue with indicator ptr

--- google/cloud/odbc/integration_tests/odbc_driver_tests/statement_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/statement_test.cc
@@ -193,24 +193,24 @@ void PutAllDataTypes(std::shared_ptr<ODBCHandles> conn,
   auto query = "INSERT INTO " + table_name + " VALUES (?, ?, ?, ?, ?)";
   EXPECT_EQ(SQLPrepare(conn->hstmt, (SQLCHAR*)query.c_str(), SQL_NTS),
             SQL_SUCCESS);
-std::cout<<"===============inside pudata 1"<<std::endl;
+  std::cout << "===============inside pudata 1" << std::endl;
   for (int i = 0; i < 5; ++i) {
     EXPECT_EQ(SQLBindParameter(conn->hstmt, i + 1, SQL_PARAM_INPUT,
                                fields[i].c_type, fields[i].sql_type, 0, 0,
                                nullptr, 0, fields[i].str_len_or_ind_ptr),
               SQL_SUCCESS);
   }
-std::cout<<"===============inside pudata 2"<<std::endl;
+  std::cout << "===============inside pudata 2" << std::endl;
   // Execute and provide data using SQLPutData
   EXPECT_EQ(SQLExecute(conn->hstmt), SQL_NEED_DATA);
   SQLPOINTER param = nullptr;
-std::cout<<"===============inside pudata 3"<<std::endl;
+  std::cout << "===============inside pudata 3" << std::endl;
   for (int i = 0; i < 5; ++i) {
     EXPECT_EQ(SQLParamData(conn->hstmt, &param), SQL_NEED_DATA);
     EXPECT_EQ(SQLPutData(conn->hstmt, fields[i].data_ptr, fields[i].data_size),
               SQL_SUCCESS);
   }
-std::cout<<"===============inside pudata 4"<<std::endl;
+  std::cout << "===============inside pudata 4" << std::endl;
   // Finalize data execution
   EXPECT_EQ(SQLParamData(conn->hstmt, nullptr), SQL_SUCCESS);
 }
@@ -226,7 +226,7 @@ void ValidateAllPutData(std::shared_ptr<ODBCHandles> conn,
             SQL_SUCCESS);
   EXPECT_EQ(SQLExecute(conn->hstmt), SQL_SUCCESS);
   EXPECT_EQ(SQLFetch(conn->hstmt), SQL_SUCCESS);
-std::cout<<"===============inside validate  1"<<std::endl;
+  std::cout << "===============inside validate  1" << std::endl;
   // Define validation fields
   SQLCHAR result_bool = 0;
   SQLLEN result_bool_len = 0;
@@ -254,7 +254,7 @@ std::cout<<"===============inside validate  1"<<std::endl;
       {result_binary, sizeof(result_binary), SQL_C_BINARY, SQL_LONGVARBINARY,
        &result_binary_len},
   };
-std::cout<<"===============inside validate 2"<<std::endl;
+  std::cout << "===============inside validate 2" << std::endl;
   // Fetch and validate data
   for (int i = 0; i < 5; ++i) {
     EXPECT_EQ(SQLGetData(conn->hstmt, i + 1, validations[i].c_type,
@@ -262,20 +262,20 @@ std::cout<<"===============inside validate 2"<<std::endl;
                          validations[i].str_len_or_ind_ptr),
               SQL_SUCCESS);
   }
-std::cout<<"===============inside validate 3"<<std::endl;
+  std::cout << "===============inside validate 3" << std::endl;
   // Assertions for validation
   EXPECT_EQ(result_bool, SQL_TRUE);
   EXPECT_EQ(result_int, 42);
   EXPECT_DOUBLE_EQ(result_float, 3.14);
-  std::cout<<"===============inside validate 4"<<std::endl;
+  std::cout << "===============inside validate 4" << std::endl;
   EXPECT_EQ(std::string((char*)result_string), "");
-std::cout<<"===============inside validate 5"<<std::endl;
+  std::cout << "===============inside validate 5" << std::endl;
   std::vector<uint8_t> expected_binary = {0xDE, 0xAD, 0xBE, 0xEF};
   EXPECT_EQ(result_binary_len, expected_binary.size());
-  std::cout<<"===============inside validate 6"<<std::endl;
+  std::cout << "===============inside validate 6" << std::endl;
   EXPECT_TRUE(std::equal(result_binary, result_binary + result_binary_len,
                          expected_binary.begin()));
-  std::cout<<"===============inside validate 7"<<std::endl;
+  std::cout << "===============inside validate 7" << std::endl;
 }
 
 TEST(StatementTest, SQLFetch_Unicode) {
@@ -2964,9 +2964,6 @@ TEST(SQLMoreResults, ErrorHandling) {
   EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
 }
 
-#ifdef _WIN32
-// TODO(b/404480454): Issue with SQLBindParameter When Using the
-//  Same Indicator Pointer for Multiple Parameters
 TEST(StatementTest, SQLPutDataStringDataChunks) {
   auto const table_name = kDatasetWithTablePrefix + "ODBC_PUT_DATA_TEST";
   Table table(table_name);
@@ -3010,7 +3007,6 @@ TEST(StatementTest, SQLPutDataStringDataChunks) {
   table.Drop(conn);
   EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
 }
-#endif  // _WIN32
 
 TEST(StatementTest, SQLPutDataErrorTest) {
   // Test SQLPutData error scenarios with proper sequence and data validation
@@ -3191,25 +3187,25 @@ TEST(StatementTest, SQLPutDataMultipleDataTypes) {
   EXPECT_EQ(Connect(kDefaultConnectionString, conn), SQL_SUCCESS);
   table.CreateWithPrepare(conn, getSchemaStr(schema));
   EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
-std::cout<<"===============adding prints before put"<<std::endl;
+  std::cout << "===============adding prints before put" << std::endl;
   // Insert and validate data
   EXPECT_EQ(Connect(kDefaultConnectionString, conn), SQL_SUCCESS);
   PutAllDataTypes(conn, table_name);
-  std::cout<<"===============adding prints after put"<<std::endl;
+  std::cout << "===============adding prints after put" << std::endl;
   EXPECT_EQ(SQLFreeStmt(conn->hstmt, SQL_CLOSE), SQL_SUCCESS);
   EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
 
   EXPECT_EQ(Connect(kDefaultConnectionString, conn), SQL_SUCCESS);
-  std::cout<<"===============adding prints before validate"<<std::endl;
+  std::cout << "===============adding prints before validate" << std::endl;
   ValidateAllPutData(conn, table_name);
-  std::cout<<"===============adding prints after validate"<<std::endl;
+  std::cout << "===============adding prints after validate" << std::endl;
   EXPECT_EQ(SQLFreeStmt(conn->hstmt, SQL_CLOSE), SQL_SUCCESS);
   EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
 
   // Clean up
-  //EXPECT_EQ(Connect(kDefaultConnectionString, conn), SQL_SUCCESS);
-  //table.DropWithPrepare(conn);
-  //EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
+  // EXPECT_EQ(Connect(kDefaultConnectionString, conn), SQL_SUCCESS);
+  // table.DropWithPrepare(conn);
+  // EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
 }
 #endif
 TEST(SQLMoreResults, ProcedureWithNoParameters) {
