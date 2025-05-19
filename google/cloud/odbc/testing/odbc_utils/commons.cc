456209bcd61f26bbb3356ab63faf37a80dfb76e1
KanchanShu <Kanchan.Shukla@37848692.corp-partner.google.com>
2025-05-19 10:10:12 +0000

    changes for intermittent issue

--- google/cloud/odbc/testing/odbc_utils/commons.cc
+++ google/cloud/odbc/testing/odbc_utils/commons.cc
@@ -1640,19 +1640,15 @@ std::string ConvertSQLWCHARToString(SQLWCHAR* in_str, SQLINTEGER in_str_len) {
   if (((in_str != nullptr) && (in_str[0] == '\0'))) {
     return std::string();
   }
-  std::wstring stmt_txt_wstr;
-  std::wstring wstr(reinterpret_cast<wchar_t const*>(in_str));
   if (in_str_len == SQL_NTS || in_str_len == NULL) {
-    in_str_len = wstr.size();
-    if (sizeof(SQLWCHAR) == 2) {
-      in_str_len = in_str_len * sizeof(SQLWCHAR);
-    }
+    in_str_len =
+        static_cast<SQLINTEGER>(std::char_traits<SQLWCHAR>::length(in_str));
   }
-  stmt_txt_wstr.reserve(in_str_len);
-  for (SQLINTEGER i = 0; i < in_str_len; ++i) {
-    stmt_txt_wstr.push_back(static_cast<wchar_t>(in_str[i]));
-  }
-  return Utf16ToUtf8(stmt_txt_wstr);
+
+  // Directly create a wide string
+  std::wstring wstr(in_str, in_str + in_str_len);
+
+  return Utf16ToUtf8(wstr);
 }
 
 std::string ConvertHexToChar(std::string const& hex_str) {
