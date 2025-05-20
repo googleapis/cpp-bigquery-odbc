5088ef52e56bc924a5710107265a1b642341318c
KanchanShu <Kanchan.Shukla@37848692.corp-partner.google.com>
2025-05-20 13:39:22 +0000

    test

--- google/cloud/odbc/testing/odbc_utils/commons.cc
+++ google/cloud/odbc/testing/odbc_utils/commons.cc
@@ -1526,7 +1526,7 @@ std::string Utf16ToUtf8(std::wstring const& utf_16_str) {
   }
 #ifdef _WIN32
   // https://learn.microsoft.com/en-us/windows/win32/api/stringapiset/nf-stringapiset-widechartomultibyte
-  int utf8Length = WideCharToMultiByte(code_page, 0, utf_16_str.c_str(), -1,
+  int utf8Length = WideCharToMultiByte(CP_ACP, 0, utf_16_str.c_str(), -1,
                                        NULL, 0, NULL, NULL);
   if (utf8Length == 0) {
     throw std::runtime_error(
@@ -1537,7 +1537,7 @@ std::string Utf16ToUtf8(std::wstring const& utf_16_str) {
   }
   std::string utf8Str(utf8Length, 0);
   // https://learn.microsoft.com/en-us/windows/win32/api/stringapiset/nf-stringapiset-widechartomultibyte
-  int result = WideCharToMultiByte(code_page, 0, utf_16_str.c_str(), -1,
+  int result = WideCharToMultiByte(CP_ACP, 0, utf_16_str.c_str(), -1,
                                    &utf8Str[0], utf8Length, NULL, NULL);
   if (result == 0) {
     throw std::runtime_error("Error while converting wstring to string");
