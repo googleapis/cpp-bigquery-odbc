f698bca4c378b257a102098f36d85629bb7586cf
KanchanShu <Kanchan.Shukla@37848692.corp-partner.google.com>
2025-05-20 14:02:36 +0000

    test

--- google/cloud/odbc/bq_driver/internal/data_translation.cc
+++ google/cloud/odbc/bq_driver/internal/data_translation.cc
@@ -164,12 +164,12 @@ odbc_internal::StatusRecord ConvertFromStringDSValue(DSValue const& src_dsval,
 
   if (dest_type == SQL_C_CHAR) {
 #ifdef _WIN32
-    auto str = Utf8ToACP(src_str);
-    if (!str.Ok()) {
+    auto acp_str = Utf8ToACP(src_str);
+    if (!acp_str.Ok()) {
       return StatusRecord{SQLStates::k_HY000(),
                           "UTF8 to ACP Conversion Failed"};
     }
-    src_str = *str;
+    return StringValueToOutputBufferResponse(acp_str->c_str(), dest_data);
 #endif
     return StringValueToOutputBufferResponse(src_str.c_str(), dest_data);
   }
@@ -923,6 +923,14 @@ StatusRecord ConvertStringToJsonOutputBuffer(std::string const& src_str,
 
   switch (dest_type) {
     case SQL_C_CHAR: {
+      #ifdef _WIN32
+    auto acp_str = Utf8ToACP(src_str);
+    if (!acp_str.Ok()) {
+      return StatusRecord{SQLStates::k_HY000(),
+                          "UTF8 to ACP Conversion Failed"};
+    }
+    return StringValueToOutputBufferResponse(acp_str->c_str(), dest_data);
+#endif
       return StringValueToOutputBufferResponse(src_str.c_str(), dest_data);
     }
     case SQL_C_WCHAR: {
@@ -980,6 +988,14 @@ StatusRecord ConvertFromArrayDSValue(DSValue const& src_dsval,
 
   switch (dest_data.type) {
     case SQL_C_CHAR: {
+      #ifdef _WIN32
+    auto acp_str = Utf8ToACP(src_str);
+    if (!acp_str.Ok()) {
+      return StatusRecord{SQLStates::k_HY000(),
+                          "UTF8 to ACP Conversion Failed"};
+    }
+    return StringValueToOutputBufferResponse(acp_str->c_str(), dest_data);
+#endif
       return StringValueToOutputBufferResponse(src_str.c_str(), dest_data);
     }
     case SQL_C_WCHAR: {
