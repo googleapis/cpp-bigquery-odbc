816674aedf139f296de6b1663935239fd004d8a3
Sachin Purohit <sachinpurohit@google.com>
2025-05-22 11:14:36 +0000

    minor: updated the comment for  unicode CP_ACP behaviour

--- google/cloud/odbc/integration_tests/odbc_driver_tests/statement_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/statement_test.cc
@@ -105,40 +105,38 @@ void VerifyColumnWiseUnicodeResults(StdUnicodeRows input_data,
     std::vector<std::string> input_col_values;
     if (col_name.compare("Hindi")) {
       for (auto data : input_data) {
-        std::string dataStr;
-        // For the existing driver in Unicode mode on Windows, data is received
-        // encoded in CP_ACP but is transmitted as UTF-8. In contrast, our
-        // driver consistently uses UTF-8 for Unicode data, as required by the
-        // BigQuery client API. Converting UTF-8 back to ACP in our case
-        // introduces an unnecessary two-step process, which can negatively
-        // impact performance.
+        std::string data_str;
+        // For the existing driver in Unicode mode on Windows, data is
+        // received(by the application) encoded in CP_ACP but is transmitted as
+        // UTF-8. In contrast, our driver consistently uses UTF-8 for Unicode
+        // data, because we don't want to conform to the legacy CP_ACP encoding
 #ifdef _WIN32
 #ifdef BQ_DRIVER_INTEGRATION_TESTS
 
-        dataStr = Utf16ToUtf8(data.str_field2);
+        data_str = Utf16ToUtf8(data.str_field2);
 #else
-        dataStr = Utf16ToUtf8(data.str_field2, CP_ACP);
+        data_str = Utf16ToUtf8(data.str_field2, CP_ACP);
 #endif  // BQ_DRIVER_INTEGRATION_TESTS
 #else
-        dataStr = Utf16ToUtf8(data.str_field2);
+        data_str = Utf16ToUtf8(data.str_field2);
 #endif  //_WIN32
-        input_col_values.emplace_back(dataStr);
+        input_col_values.emplace_back(data_str);
       }
     } else if (col_name.compare("Chinese")) {
       for (auto data : input_data) {
-        std::string dataStr;
+        std::string data_str;
 #ifdef _WIN32
 #ifdef BQ_DRIVER_INTEGRATION_TESTS
 
-        dataStr = Utf16ToUtf8(data.str_field1);
+        data_str = Utf16ToUtf8(data.str_field1);
 #else
-        dataStr = Utf16ToUtf8(data.str_field1, CP_ACP);
+        data_str = Utf16ToUtf8(data.str_field1, CP_ACP);
 #endif  // BQ_DRIVER_INTEGRATION_TESTS
 #else
-        dataStr = Utf16ToUtf8(data.str_field1);
+        data_str = Utf16ToUtf8(data.str_field1);
 #endif  //_WIN32
 
-        input_col_values.emplace_back(dataStr);
+        input_col_values.emplace_back(data_str);
       }
     }
     sort(input_col_values.begin(), input_col_values.end(), str_comparison);
