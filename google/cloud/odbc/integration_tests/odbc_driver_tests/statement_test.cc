bc0807b7bc9c748872a2f28ed57d17262a6beab9
sgunji25 <suresh.gunji@cloudsufi.com.test-google-a.com>
2025-05-27 17:41:51 +0000

    impl(bq_driver): resolve comments

--- google/cloud/odbc/integration_tests/odbc_driver_tests/statement_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/statement_test.cc
@@ -165,7 +165,7 @@ void PutAllDataTypes(std::shared_ptr<ODBCHandles> conn,
   SQLCHAR bool_data = SQL_TRUE;
   SQLLEN bool_len = SQL_DATA_AT_EXEC;
 
-  SQLLEN int_data = 42;
+  SQLBIGINT int_data = 42;
   SQLLEN int_len = SQL_DATA_AT_EXEC;
 
   double float_data = 3.14;
@@ -179,7 +179,7 @@ void PutAllDataTypes(std::shared_ptr<ODBCHandles> conn,
 
   DataField fields[] = {
       {&bool_data, sizeof(bool_data), SQL_C_BIT, SQL_BIT, &bool_len},
-      {&int_data, sizeof(int_data), SQL_C_SLONG, SQL_INTEGER, &int_len},
+      {&int_data, sizeof(int_data), SQL_C_SBIGINT, SQL_BIGINT, &int_len},
       {&float_data, sizeof(float_data), SQL_C_DOUBLE, SQL_DOUBLE, &float_len},
       {(SQLPOINTER)text_data.c_str(), static_cast<SQLLEN>(text_data.size()),
        SQL_C_CHAR, SQL_LONGVARCHAR, &string_len},
@@ -243,7 +243,7 @@ void ValidateAllPutData(std::shared_ptr<ODBCHandles> conn,
 
   DataField validations[] = {
       {&result_bool, sizeof(result_bool), SQL_C_BIT, SQL_BIT, &result_bool_len},
-      {&result_int, sizeof(result_int), SQL_C_SLONG, SQL_INTEGER,
+      {&result_int, sizeof(result_int), SQL_C_SBIGINT, SQL_BIGINT,
        &result_int_len},
       {&result_float, sizeof(result_float), SQL_C_DOUBLE, SQL_DOUBLE,
        &result_float_len},
@@ -3162,14 +3162,6 @@ TEST(StatementTest, SQLPutDataMultipleDataTypes) {
   auto const table_name =
       kDatasetWithTablePrefix + "ODBC_PUT_DATA_MULTIPLE_TYPES_TEST";
   Table table(table_name);
-  std::cout << "========sizeof(SQLLEN)" << sizeof(SQLLEN) << std::endl;
-  std::cout << "=======sizeof(int64_t)" << sizeof(int64_t) << std::endl;
-  std::cout << "========sizeof(SQL_INTEGER)" << sizeof(SQL_INTEGER)
-            << std::endl;
-  std::cout << "=======sizeof(SQL_C_LONG)" << sizeof(SQL_C_LONG) << std::endl;
-  std::cout << "========sizeof(SQL_C_SBIGINT)" << sizeof(SQL_C_SBIGINT)
-            << std::endl;
-  std::cout << "=======sizeof(SQL_BIGINT)" << sizeof(SQL_BIGINT) << std::endl;
   Schema schema{{"BoolField", "BOOL"},
                 {"IntField", "INT64"},
                 {"FloatField", "FLOAT64"},
