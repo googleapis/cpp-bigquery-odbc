09063170a95ef791ec588effe262e53aa09ecfb8
KanchanShu <Kanchan.Shukla@37848692.corp-partner.google.com>
2025-06-09 10:00:15 +0000

    Revert "test"
    This reverts commit 80036039f3bb6b7d4090acf3602925db474d9bbd.

--- ci/cloudbuild/builds/integration-tests.sh
+++ ci/cloudbuild/builds/integration-tests.sh
@@ -42,7 +42,6 @@ mapfile -t cmake_args < <(cmake::common_args)
 io::run cmake "${cmake_args[@]}" \
   -DCMAKE_CXX_STANDARD=17 \
   -DODBC_INTEGRATION_TESTING=OFF \
-  -DUSE_SANITIZER=Address \
   -DODBC_DEMO_TESTING=OFF \
   -DODBC_UNIT_TESTING=OFF \
   -DCLIENT_LIBRARY_INTEGRATION_TESTING=ON
