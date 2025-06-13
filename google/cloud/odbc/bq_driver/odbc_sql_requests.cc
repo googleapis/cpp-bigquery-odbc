0d41a3004de27eaa4f7bbfef3fa8c6554387abc1
KanchanShu <Kanchan.Shukla@37848692.corp-partner.google.com>
2025-06-13 13:02:51 +0000

    Modified functinality and added testcases

--- google/cloud/odbc/bq_driver/odbc_sql_requests.cc
+++ google/cloud/odbc/bq_driver/odbc_sql_requests.cc
@@ -287,8 +287,7 @@ StatusRecord ActuallyProcessExecute(StatementHandle& stmt_handle,
   }
 
   // Process DSResults into a ResultSet
-  auto rs_status_record_or = ProcessQueryResults(
-      *ds_status_record_or, conn_handle.GetDsn().default_string_column_length);
+  auto rs_status_record_or = ProcessQueryResults(*ds_status_record_or);
   if (!rs_status_record_or) {
     stmt_handle.SetStmtState(failure_state);
     return rs_status_record_or.GetStatusRecord();
@@ -431,8 +430,7 @@ StatusRecord ActuallyGetMoreResults(StatementHandle& stmt_handle) {
   stmt_handle.SetDSResults(results);
 
   // Process query results into a result set if it's a SELECT statement.
-  auto rs_status_record_or = ProcessQueryResults(
-      results, conn_handle.GetDsn().default_string_column_length);
+  auto rs_status_record_or = ProcessQueryResults(results);
   auto max_rows_status = stmt_handle.GetAttribute(SQL_ATTR_MAX_ROWS);
   if (!max_rows_status) {
     return max_rows_status.GetStatusRecord();
