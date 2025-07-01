c8bc3f2aefb9e2d49b2b826295b839ac2d216363
KanchanShu <Kanchan.Shukla@37848692.corp-partner.google.com>
2025-07-01 11:30:00 +0000

    test

--- google/cloud/odbc/integration_tests/odbc_driver_tests/catalog_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/catalog_test.cc
@@ -1759,6 +1759,7 @@ TEST(SQLColumns, Check_SQLColumnsDescriptors) {
     ASSERT_NE(it, kColumnMetaDataSchema.end());
     EXPECT_EQ(col_name, *it);
   }
+  EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
 }
 
 TEST(SQLTables, Check_SQLTablesDescriptors) {
