e722ab42cb267abc313af6354c4fabb6e0443994
KanchanShu <Kanchan.Shukla@37848692.corp-partner.google.com>
2025-07-01 12:48:42 +0000

    test

--- google/cloud/odbc/integration_tests/odbc_driver_tests/connection_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/connection_test.cc
@@ -975,6 +975,7 @@ TEST(ConnectionTest, SQLBrowseConnect_WithDsn) {
     EXPECT_EQ(res_out_conn_str, expected_conn_out_str);
     EXPECT_EQ(out_conn_str_len, expected_conn_out_str.size());
   }
+  EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
 }
 
 TEST(ConnectionTest, SQLBrowseConnect_OverrideDSNWithConnStrValues) {
