9c7b3aa80e5556f0cde0ef41514c079797a20724
KanchanShu <Kanchan.Shukla@37848692.corp-partner.google.com>
2025-07-03 06:20:50 +0000

    tset

--- google/cloud/odbc/integration_tests/odbc_driver_tests/connection_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/connection_test.cc
@@ -1097,10 +1097,15 @@ TEST(ConnectionTest, SQLBrowseConnect_SQL_NEED_DATA) {
   EXPECT_THAT(res_out_conn_str,
               HasSubstr("Catalog:Catalog=?;OAuthMechanism:OAuthMechanism=?"));
 #endif  // _WIN32
-EXPECT_EQ(SQLFreeHandle(SQL_HANDLE_DBC, conn->hdbc), SQL_SUCCESS);
-EXPECT_EQ(SQLFreeHandle(SQL_HANDLE_ENV, conn->henv), SQL_SUCCESS);
-conn->hdbc = nullptr;
-conn->henv = nullptr;
+if (conn->hdbc != nullptr) {
+  EXPECT_EQ(SQLFreeHandle(SQL_HANDLE_DBC, conn->hdbc), SQL_SUCCESS);
+  conn->hdbc = nullptr;
+}
+
+if (conn->henv != nullptr) {
+  EXPECT_EQ(SQLFreeHandle(SQL_HANDLE_ENV, conn->henv), SQL_SUCCESS);
+  conn->henv = nullptr;
+}
 }
 
 TEST(ConnectionTest, SQLBrowseConnect_StringDataRightTruncated) {
@@ -1167,10 +1172,16 @@ TEST(ConnectionTest, SQLBrowseConnect_InvalidConnectionAttribute) {
                 HasSubstr("Catalog:Catalog=?;OAuthMechanism:OAuthMechanism=?"));
 #endif  // _WIN32
   }
-EXPECT_EQ(SQLFreeHandle(SQL_HANDLE_DBC, conn->hdbc), SQL_SUCCESS);
-EXPECT_EQ(SQLFreeHandle(SQL_HANDLE_ENV, conn->henv), SQL_SUCCESS);
-conn->hdbc = nullptr;
-conn->henv = nullptr;
+  if (conn->hdbc != nullptr) {
+    EXPECT_EQ(SQLFreeHandle(SQL_HANDLE_DBC, conn->hdbc), SQL_SUCCESS);
+    conn->hdbc = nullptr;
+  }
+  
+  if (conn->henv != nullptr) {
+    EXPECT_EQ(SQLFreeHandle(SQL_HANDLE_ENV, conn->henv), SQL_SUCCESS);
+    conn->henv = nullptr;
+  }
+  
 }
 
 TEST(ConnectionTest, SQLBrowseConnect_InvalidConnectionString) {
