37fe15f732b56f3cf1a335e46c9acaa1472fdb4a
Anshu6250 <anshu@37848692.corp-partner.google.com>
2025-07-15 10:06:55 +0000

    changes

--- google/cloud/odbc/bq_client_interface/odbc_bq_client_test.cc
+++ google/cloud/odbc/bq_client_interface/odbc_bq_client_test.cc
@@ -21,5 +21,19 @@ namespace google::cloud::odbc_bigquery_client_interface {
 
 using google::cloud::internal::GetEnv;
 using google::cloud::odbc_bigquery_client_interface::ODBCBQClient;
+using google::cloud::odbc_testing_utils::StatusRecordIs;
+using ::testing::HasSubstr;
 
+TEST(ODBCBQClient, CreateBQClientFailsWithInvalidCredentials) {
+  auto odbc_bq_client = ODBCBQClient::CreateBQClient(
+      {OauthMechanism::kServiceAndUserAccount, ""});
+
+  auto const expected_sql_state = odbc_internal::SQLStates::k_HY000();
+  auto const expected_message =
+      HasSubstr("The path to the file can't be empty");
+  auto const expected_status =
+      StatusRecordIs(expected_sql_state, expected_message);
+
+  EXPECT_THAT(odbc_bq_client, expected_status);
+}
 }  // namespace google::cloud::odbc_bigquery_client_interface
