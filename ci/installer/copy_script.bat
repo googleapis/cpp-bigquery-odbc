89f7a3efa63a2a7af5fa81072cf5e3a2888bdf5e
Neeraj Dwivedii <neeraj.dwivedi@37848692.corp-partner.google.com>
2025-08-04 13:17:57 +0000

    feat(bq_driver): Update installer with static library linking

--- ci/installer/copy_script.bat
+++ ci/installer/copy_script.bat
@@ -6,7 +6,7 @@ set "DEST_DIR=files\x64\Release"
 
 if not exist "%DEST_DIR%" mkdir "%DEST_DIR%"
 
-set "FILES=abseil_dll.dll cares.dll google_cloud_odbc_bq_driver.dll libcrypto-3-x64.dll libcurl.dll libprotobuf.dll libssl-3-x64.dll re2.dll zlib1.dll"
+set "FILES=google_cloud_odbc_bq_driver.dll"
 
 for %%F in (%FILES%) do (
     if exist "%SOURCE_DIR%\%%F" (
