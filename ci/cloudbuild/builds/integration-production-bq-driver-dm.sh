65d34a6554f60d7475d7c6538fd462ea98a41131
Neeraj Dwivedii <neeraj.dwivedi@37848692.corp-partner.google.com>
2025-08-06 15:19:32 +0000

    impl(bq_driver): test check

--- ci/cloudbuild/builds/integration-production-bq-driver-dm.sh
+++ ci/cloudbuild/builds/integration-production-bq-driver-dm.sh
@@ -39,7 +39,6 @@ BUILD_DIR="/opt/odbc-driver"
 # This is the name of DSN set in odbc.ini
 export ODBC_TESTS_DSN="SampleDSNGoogleDriver"
 export CPP_BIGQUERY_ODBC_TEST_TABLE_PREFIX=${TRIGGER_NAME//[-:;.,?]/_}_${BRANCH_NAME//[-:;.,?]/_}
-
 # Check if unixODBC is installed
 if command -v odbcinst &>/dev/null; then
   # unixODBC is installed, export environment variable
