5d8f8df5f93eedfd5c2c555c7c799c082a5d532f
Neeraj Dwivedii <neeraj.dwivedi@37848692.corp-partner.google.com>
2025-11-03 06:57:52 +0000

    test

--- ci/cloudbuild/builds/integration-production-bq-driver-dm.sh
+++ ci/cloudbuild/builds/integration-production-bq-driver-dm.sh
@@ -56,6 +56,7 @@ io::run cmake -B "$BUILD_DIR" \
   "${cmake_args[@]}" \
   -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" \
   -DCMAKE_CXX_STANDARD=17 \
+  -DENABLE_SANITIZER=ON \
   -DODBC_INTEGRATION_TESTING=ON \
   -DBQ_DRIVER_INTEGRATION_TESTS=ON \
   -DODBC_DEMO_TESTING=ON \
