8709cccd46149f46da7003165244cc597fa3327c
Neeraj Dwivedii <neeraj.dwivedi@37848692.corp-partner.google.com>
2025-11-21 09:25:59 +0000

    update gcc version on docker file for filesystem pkg

--- google/cloud/odbc/bq_driver.cmake
+++ google/cloud/odbc/bq_driver.cmake
@@ -225,7 +225,6 @@ target_link_libraries(
             absl::base)
 
 target_compile_features(google_cloud_odbc_bq_driver PUBLIC cxx_std_17)
-
 set_target_properties(
     google_cloud_odbc_bq_driver
     PROPERTIES EXPORT_NAME google-cloud-odbc::bq-driver
