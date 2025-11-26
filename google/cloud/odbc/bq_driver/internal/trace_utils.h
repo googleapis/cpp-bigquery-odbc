0c5db31f40fd27d25a0f1c1366ad1ea39ef69b57
Neeraj Dwivedii <neeraj.dwivedi@37848692.corp-partner.google.com>
2025-11-26 07:04:46 +0000

    fix variable

--- google/cloud/odbc/bq_driver/internal/trace_utils.h
+++ google/cloud/odbc/bq_driver/internal/trace_utils.h
@@ -109,8 +109,8 @@ struct TraceOptions {
   bool logging_enabled;
   bool is_file_closed;
   int log_level{0};
-  int max_file_size{50};   // max number of log files (50).
-  int max_file_count{20};  // max file size of a single file(20 MB)
+  int max_file_count{50};  // max number of log files (50).
+  int max_file_size{20};   // max file size of a single file(20 MB)
   int current_file_index{0};
   std::string log_path;
   std::string log_file;
