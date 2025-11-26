2ee88ee005691ec44be01db63853364d3ee727a3
Anshu6250 <anshu@37848692.corp-partner.google.com>
2025-11-26 08:33:07 +0000

    structured the test cases

--- google/cloud/odbc/integration_tests/odbc_driver_tests/statement_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/statement_test.cc
@@ -94,7 +94,46 @@ void CheckColumnData(std::shared_ptr<ODBCHandles> conn, std::string table_name,
     EXPECT_EQ(col_ptr->nullable, SQL_NULLABLE);
   }
 }
+struct ExpectedCol {
+  char const* name;
+  SQLSMALLINT type;
+  SQLULEN size;
+  SQLSMALLINT decimals;
+  SQLSMALLINT nullable;
+};
 
+void VerifyResultSetMetadata(SQLHSTMT hstmt,
+                             SQLSMALLINT expected_col_count,
+                             ExpectedCol const* expected_cols) {
+  SQLSMALLINT col_count = 0;
+  SQLRETURN ret = SQLNumResultCols(hstmt, &col_count);
+  ASSERT_TRUE(SQL_SUCCEEDED(ret));
+  EXPECT_EQ(col_count, expected_col_count);
+
+  for (SQLSMALLINT i = 1; i <= col_count; ++i) {
+    SQLCHAR col_name[256] = {0};
+    SQLSMALLINT name_len = 0;
+    SQLSMALLINT data_type = 0;
+    SQLSMALLINT decimal_digits = 0;
+    SQLSMALLINT nullable = 0;
+    SQLULEN col_size = 0;
+
+    ret = SQLDescribeCol(hstmt, i, col_name, sizeof(col_name), &name_len,
+                         &data_type, &col_size, &decimal_digits, &nullable);
+    ASSERT_TRUE(SQL_SUCCEEDED(ret));
+
+    auto const& exp = expected_cols[i - 1];
+
+    EXPECT_STREQ(reinterpret_cast<char const*>(col_name), exp.name)
+        << "Column " << i << " name mismatch";
+    EXPECT_EQ(data_type, exp.type) << "Column " << i << " type mismatch";
+    EXPECT_EQ(col_size, exp.size) << "Column " << i << " size mismatch";
+    EXPECT_EQ(decimal_digits, exp.decimals)
+        << "Column " << i << " decimal digits mismatch";
+    EXPECT_EQ(nullable, exp.nullable)
+        << "Column " << i << " nullable flag mismatch";
+  }
+}
 // Verify if the inserted data(<input_data>) is the same as the data fetched
 // col-wise Note: This doesn't verify the integrity of the fetched rows
 void VerifyColumnWiseUnicodeResults(StdUnicodeRows input_data,
@@ -680,55 +719,24 @@ TEST(StatementTest, Check_SQL_Primary_key) {
   ASSERT_EQ(Connect(kDefaultConnectionString, conn), SQL_SUCCESS);
 
   SQLRETURN ret = SQLPrimaryKeys(
-      conn->hstmt, (SQLCHAR*)"bigquery-devtools-drivers", SQL_NTS,
-      (SQLCHAR*)"INTEGRATION_TESTS", SQL_NTS, (SQLCHAR*)"Test_Table", SQL_NTS);
+      conn->hstmt,
+      (SQLCHAR*)"bigquery-devtools-drivers", SQL_NTS,
+      (SQLCHAR*)"INTEGRATION_TESTS", SQL_NTS,
+      (SQLCHAR*)"Test_Table", SQL_NTS);
   ASSERT_TRUE(SQL_SUCCEEDED(ret));
-
-  SQLSMALLINT col_count = 0;
-  ret = SQLNumResultCols(conn->hstmt, &col_count);
-  ASSERT_TRUE(SQL_SUCCEEDED(ret));
-  EXPECT_EQ(col_count, 6);
-
-  struct ExpectedCol {
-    char const* name;
-    SQLSMALLINT type;
-    SQLULEN size;
-    SQLSMALLINT decimals;
-    SQLSMALLINT nullable;
-  };
 
   ExpectedCol expected[] = {
-      {"TABLE_CAT", SQL_WVARCHAR, 128, 0, SQL_NULLABLE},
+      {"TABLE_CAT",   SQL_WVARCHAR, 128,  0, SQL_NULLABLE},
       {"TABLE_SCHEM", SQL_WVARCHAR, 1024, 0, SQL_NULLABLE},
-      {"TABLE_NAME", SQL_WVARCHAR, 1024, 0, SQL_NO_NULLS},
-      {"COLUMN_NAME", SQL_WVARCHAR, 128, 0, SQL_NO_NULLS},
-      {"KEY_SEQ", SQL_SMALLINT, 5, 0, SQL_NO_NULLS},
-      {"PK_NAME", SQL_WVARCHAR, 128, 0, SQL_NULLABLE},
+      {"TABLE_NAME",  SQL_WVARCHAR, 1024, 0, SQL_NO_NULLS},
+      {"COLUMN_NAME", SQL_WVARCHAR, 128,  0, SQL_NO_NULLS},
+      {"KEY_SEQ",     SQL_SMALLINT, 5,    0, SQL_NO_NULLS},
+      {"PK_NAME",     SQL_WVARCHAR, 128,  0, SQL_NULLABLE},
   };
 
-  for (SQLSMALLINT i = 1; i <= col_count; ++i) {
-    SQLCHAR col_name[256] = {0};
-    SQLSMALLINT name_len = 0;
-    SQLSMALLINT data_type = 0;
-    SQLSMALLINT decimal_digits = 0;
-    SQLSMALLINT nullable = 0;
-    SQLULEN col_size = 0;
-
-    ret = SQLDescribeCol(conn->hstmt, i, col_name, sizeof(col_name), &name_len,
-                         &data_type, &col_size, &decimal_digits, &nullable);
-    ASSERT_TRUE(SQL_SUCCEEDED(ret));
-
-    auto const& exp = expected[i - 1];
-
-    EXPECT_STREQ(reinterpret_cast<char const*>(col_name), exp.name)
-        << "Column " << i << " name mismatch";
-    EXPECT_EQ(data_type, exp.type) << "Column " << i << " type mismatch";
-    EXPECT_EQ(col_size, exp.size) << "Column " << i << " size mismatch";
-    EXPECT_EQ(decimal_digits, exp.decimals)
-        << "Column " << i << " decimal digits mismatch";
-    EXPECT_EQ(nullable, exp.nullable)
-        << "Column " << i << " nullable flag mismatch";
-  }
+  VerifyResultSetMetadata(conn->hstmt,
+                          static_cast<SQLSMALLINT>(std::size(expected)),
+                          expected);
 
   EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
 }
@@ -738,64 +746,35 @@ TEST(StatementTest, Check_SQL_Foreign_key) {
   ASSERT_EQ(Connect(kDefaultConnectionString, conn), SQL_SUCCESS);
 
   SQLRETURN ret = SQLForeignKeys(
-      conn->hstmt, (SQLCHAR*)"bigquery-devtools-drivers", SQL_NTS,
-      (SQLCHAR*)"INTEGRATION_TESTS", SQL_NTS, (SQLCHAR*)"Test_Table", SQL_NTS,
-      NULL, 0, NULL, 0, NULL, 0);
+      conn->hstmt,
+      (SQLCHAR*)"bigquery-devtools-drivers", SQL_NTS,
+      (SQLCHAR*)"INTEGRATION_TESTS", SQL_NTS,
+      (SQLCHAR*)"Test_Table", SQL_NTS,
+      NULL, 0,
+      NULL, 0,
+      NULL, 0);
   ASSERT_TRUE(SQL_SUCCEEDED(ret));
-
-  SQLSMALLINT col_count = 0;
-  ret = SQLNumResultCols(conn->hstmt, &col_count);
-  ASSERT_TRUE(SQL_SUCCEEDED(ret));
-  EXPECT_EQ(col_count, 14);
-
-  struct ExpectedCol {
-    char const* name;
-    SQLSMALLINT type;
-    SQLULEN size;
-    SQLSMALLINT decimals;
-    SQLSMALLINT nullable;
-  };
 
   ExpectedCol expected[] = {
-      {"PKTABLE_CAT", SQL_WVARCHAR, 128, 0, SQL_NULLABLE},
+      {"PKTABLE_CAT",   SQL_WVARCHAR, 128,  0, SQL_NULLABLE},
       {"PKTABLE_SCHEM", SQL_WVARCHAR, 1024, 0, SQL_NULLABLE},
-      {"PKTABLE_NAME", SQL_WVARCHAR, 1024, 0, SQL_NO_NULLS},
-      {"PKCOLUMN_NAME", SQL_WVARCHAR, 128, 0, SQL_NO_NULLS},
-      {"FKTABLE_CAT", SQL_WVARCHAR, 128, 0, SQL_NULLABLE},
+      {"PKTABLE_NAME",  SQL_WVARCHAR, 1024, 0, SQL_NO_NULLS},
+      {"PKCOLUMN_NAME", SQL_WVARCHAR, 128,  0, SQL_NO_NULLS},
+      {"FKTABLE_CAT",   SQL_WVARCHAR, 128,  0, SQL_NULLABLE},
       {"FKTABLE_SCHEM", SQL_WVARCHAR, 1024, 0, SQL_NULLABLE},
-      {"FKTABLE_NAME", SQL_WVARCHAR, 1024, 0, SQL_NO_NULLS},
-      {"FKCOLUMN_NAME", SQL_WVARCHAR, 128, 0, SQL_NO_NULLS},
-      {"KEY_SEQ", SQL_SMALLINT, 5, 0, SQL_NO_NULLS},
-      {"UPDATE_RULE", SQL_SMALLINT, 5, 0, SQL_NULLABLE},
-      {"DELETE_RULE", SQL_SMALLINT, 5, 0, SQL_NULLABLE},
-      {"FK_NAME", SQL_WVARCHAR, 128, 0, SQL_NULLABLE},
-      {"PK_NAME", SQL_WVARCHAR, 128, 0, SQL_NULLABLE},
-      {"DEFERRABILITY", SQL_SMALLINT, 5, 0, SQL_NULLABLE},
+      {"FKTABLE_NAME",  SQL_WVARCHAR, 1024, 0, SQL_NO_NULLS},
+      {"FKCOLUMN_NAME", SQL_WVARCHAR, 128,  0, SQL_NO_NULLS},
+      {"KEY_SEQ",       SQL_SMALLINT, 5,    0, SQL_NO_NULLS},
+      {"UPDATE_RULE",   SQL_SMALLINT, 5,    0, SQL_NULLABLE},
+      {"DELETE_RULE",   SQL_SMALLINT, 5,    0, SQL_NULLABLE},
+      {"FK_NAME",       SQL_WVARCHAR, 128,  0, SQL_NULLABLE},
+      {"PK_NAME",       SQL_WVARCHAR, 128,  0, SQL_NULLABLE},
+      {"DEFERRABILITY", SQL_SMALLINT, 5,    0, SQL_NULLABLE},
   };
 
-  for (SQLSMALLINT i = 1; i <= col_count; ++i) {
-    SQLCHAR col_name[256] = {0};
-    SQLSMALLINT name_len = 0;
-    SQLSMALLINT data_type = 0;
-    SQLSMALLINT decimal_digits = 0;
-    SQLSMALLINT nullable = 0;
-    SQLULEN col_size = 0;
-
-    ret = SQLDescribeCol(conn->hstmt, i, col_name, sizeof(col_name), &name_len,
-                         &data_type, &col_size, &decimal_digits, &nullable);
-    ASSERT_TRUE(SQL_SUCCEEDED(ret));
-
-    auto const& exp = expected[i - 1];
-
-    EXPECT_STREQ(reinterpret_cast<char const*>(col_name), exp.name)
-        << "Column " << i << " name mismatch";
-    EXPECT_EQ(data_type, exp.type) << "Column " << i << " type mismatch";
-    EXPECT_EQ(col_size, exp.size) << "Column " << i << " size mismatch";
-    EXPECT_EQ(decimal_digits, exp.decimals)
-        << "Column " << i << " decimal digits mismatch";
-    EXPECT_EQ(nullable, exp.nullable)
-        << "Column " << i << " nullable flag mismatch";
-  }
+  VerifyResultSetMetadata(conn->hstmt,
+                          static_cast<SQLSMALLINT>(std::size(expected)),
+                          expected);
 
   EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
 }
