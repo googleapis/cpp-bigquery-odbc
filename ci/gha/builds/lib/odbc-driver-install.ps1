194124f8d72306a7d88b128909bade7c2f85ce8a
Neeraj Dwivedi <neeraj.dwivedi@37848692.corp-partner.google.com>
2026-01-28 14:31:19 +0530

    tempg

--- ci/gha/builds/lib/odbc-driver-install.ps1
+++ ci/gha/builds/lib/odbc-driver-install.ps1
@@ -27,6 +27,7 @@ if ($CI_CLOUDBUILD_BUILDS_LIB_ODBC_DRIVER_INSTALL_SH__ -ne $null -and ++$CI_CLOU
 # Set Environment Variables
 $env:GCS_BUCKET = "bq-dev-tools-testing-drivers"
 $env:ODBC_DRIVER_VERSION = "3.1.6.1026"
+$env:ODBC_GOOGLE_DRIVER_VERSION = "3.1.6.1026"
 if ([string]::IsNullOrEmpty($env:DRIVER_ARCH)) {
     throw "DRIVER_ARCH environment variable is not set or empty. Please provide a valid architecture."
 }
@@ -42,7 +43,7 @@ if ($env:DRIVER_ARCH -eq 'x64') {
 if($env:shard -eq 'core'){
 $env:ODBC_DRIVER_MSI_NAME = "SimbaODBCDriverforGoogleBigQuery${arch}_${env:ODBC_DRIVER_VERSION}.msi"
 }else{
-$env:ODBC_DRIVER_MSI_NAME = "ODBCDriverforBigQuery_windows_${arch}_${env:ODBC_DRIVER_VERSION}.msi"
+$env:ODBC_DRIVER_MSI_NAME = "ODBCDriverforBigQuery_windows_${arch}_${env:ODBC_GOOGLE_DRIVER_VERSION}.msi"
 }
 
 # Download from Google Cloud Storage (gsutil equivalent)
