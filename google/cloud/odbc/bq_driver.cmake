4b169fee170ad35908cfa58ec83d8be84e9526dd
Anshu6250 <anshu@37848692.corp-partner.google.com>
2026-02-02 10:39:26 +0000

    test

--- google/cloud/odbc/bq_driver.cmake
+++ google/cloud/odbc/bq_driver.cmake
@@ -301,7 +301,7 @@ function (bq_driver_define_unit_tests)
         google_cloud_odbc_bq_driver_unit_tests google_cloud_odbc_testing_utils
         google_cloud_odbc_testing_bq_driver_utils GTest::gtest 
         fuzztest::fuzztest
-fuzztest::fuzztest_gtest_main
+        fuzztest::fuzztest_gtest_main
 )
 
     target_compile_features(google_cloud_odbc_bq_driver_unit_tests
