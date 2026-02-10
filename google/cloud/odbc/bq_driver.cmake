5569e7199190e4f6fec6b51aa72ebed673ebd74a
Neeraj Dwivedi <neeraj.dwivedi@37848692.corp-partner.google.com>
2026-02-10 13:52:32 +0530

    testing new to

--- google/cloud/odbc/bq_driver.cmake
+++ google/cloud/odbc/bq_driver.cmake
@@ -299,7 +299,6 @@ function (bq_driver_define_unit_tests)
             google_cloud_odbc_testing_utils
             google_cloud_odbc_testing_bq_driver_utils
             GTest::gtest
-            fuzztest::fuzztest
             fuzztest::fuzztest_gtest_main)
     endif ()
 
