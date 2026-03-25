60bfc3242472ce5b21e5e9098131824cab6512ee
Neeraj Dwivedi <neeraj.dwivedi@37848692.corp-partner.google.com>
2026-03-25 18:20:37 +0530

    make same pipline

--- google/cloud/odbc/bq_driver/internal/odbc_type_utils_test.cc
+++ google/cloud/odbc/bq_driver/internal/odbc_type_utils_test.cc
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include "google/cloud/odbc/bq_driver/internal/odbc_type_utils.h"
+#include "google/cloud/odbc/bq_driver/internal/utils.h"
 #include "google/cloud/odbc/internal/diagnostic_records.h"
 #include <gtest/gtest.h>
 
@@ -299,7 +300,7 @@ TEST(WStrToOutputBufferResponse, SuccessWhenDestBufferLenGreaterThanSrcLen) {
       expected, dest, buffer_len, expected.size(), 0, &res_len);
 
   ASSERT_TRUE(status_record.ok());
-  std::wstring actual(dest);
+  std::wstring actual = SQLWcharToWstring(dest);
   EXPECT_EQ(L"sample-test", actual);
   EXPECT_EQ(res_len, expected.size() * sizeof(SQLWCHAR));
 }
@@ -317,7 +318,7 @@ TEST(WStrToOutputBufferResponse,
   ASSERT_FALSE(status_record.ok());
   EXPECT_EQ(SQLStates::k_01004(), status_record.sql_state);
   EXPECT_EQ("Data truncated", status_record.message);
-  std::wstring actual = reinterpret_cast<SQLWCHAR*>(dest);
+  std::wstring actual = SQLWcharToWstring(dest);
   EXPECT_EQ(L"samp", actual);
   EXPECT_EQ(res_len, (buffer_len * sizeof(SQLWCHAR)));
 }
@@ -334,7 +335,7 @@ TEST(WStrToOutputBufferResponse, SuccessWithInfoWhenDestBufferLenEqualsSrcLen) {
   ASSERT_FALSE(status_record.ok());
   EXPECT_EQ(SQLStates::k_01004(), status_record.sql_state);
   EXPECT_EQ("Data truncated", status_record.message);
-  std::wstring actual = reinterpret_cast<SQLWCHAR*>(dest);
+  std::wstring actual = SQLWcharToWstring(dest);
   EXPECT_EQ(L"samp", actual);
   EXPECT_EQ(res_len, (buffer_len * sizeof(SQLWCHAR)));
 }
@@ -349,7 +350,7 @@ TEST(WStrToOutputBufferResponse, SuccessWhenStcLenLenIsZero) {
       expected, dest, buffer_len, expected.size(), 0, &res_len);
 
   ASSERT_TRUE(status_record.ok());
-  std::wstring actual = reinterpret_cast<SQLWCHAR*>(dest);
+  std::wstring actual = SQLWcharToWstring(dest);
   EXPECT_EQ(L"", actual);
   EXPECT_EQ(0, res_len);
 }
