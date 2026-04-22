20a4de0d8f1fe56f8a7b7611621b586ae1cdc0a9
Khushi Kathuria <khushi.kathuria@37848692.corp-partner.google.com>
2026-04-22 15:45:32 +0530

    fix(bq_driver): Log Form Fix

--- google/cloud/odbc/bq_driver/odbc_windows.cc
+++ google/cloud/odbc/bq_driver/odbc_windows.cc
@@ -90,7 +90,7 @@ std::string ConvertLogLevel(std::string log_level) {
   } else if (log_level == "LOG_WARNING") {
     log_level_val = std::to_string(static_cast<int>(LogLevel::kLogWarning));
   } else {
-    log_level_val = std::to_string(static_cast<int>(LogLevel::kLogInfo));
+    log_level_val = std::to_string(static_cast<int>(LogLevel::kLogOff));
   }
   return log_level_val;
 }
