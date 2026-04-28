46336ad8c2ffe3e8177a9710cd34993bc25cc795
Khushi Kathuria <khushi.kathuria@37848692.corp-partner.google.com>
2026-04-28 11:27:28 +0530

    Test asan

--- ci/cloudbuild/builds/integration-bq-driver.sh
+++ ci/cloudbuild/builds/integration-bq-driver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2024 Google LLC
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,22 +25,56 @@ source module ci/cloudbuild/builds/lib/secrets.sh
 source module ci/cloudbuild/builds/lib/unit-tests.sh
 source module ci/lib/io.sh
 
-# This runs all the unit tests
+WORKSPACE_DIR=$(pwd)
 
+# Export as env variable
+VCPKG_VERSION=$(cat /tmp/vcpkg-version.txt)
+export VCPKG_VERSION
+echo "Using VCPKG_VERSION=$VCPKG_VERSION"
+
+# Vcpkg install and configure
+export VCPKG_ROOT=/vcpkg
+git clone --branch "$VCPKG_VERSION" https://github.com/microsoft/vcpkg.git "$VCPKG_ROOT"
+cd "$VCPKG_ROOT"
+git checkout "$VCPKG_VERSION"
+
+# Bootstrap
+./bootstrap-vcpkg.sh -disableMetrics
+
+cd "$WORKSPACE_DIR"
+
+# This runs all the unit tests
 mapfile -t args < <(bazel::common_args)
 mapfile -t unit_tests_args < <(unit_tests::bazel_args)
 mapfile -t secrets_bazel < <(secrets::bazel_args)
 
 io::run bazel test "${args[@]}" "${secrets_bazel[@]}" "${unit_tests_args[@]}" --test_tag_filters=unit-tests ...
 
-# Run the integration tests for google driver
+# Run the integration tests
 mapfile -t cmake_args < <(cmake::common_args)
 
-# This is the name of DSN set in odbc.ini from simba.zip
-export ODBC_TESTS_DSN="SampleDSN"
-export CPP_BIGQUERY_ODBC_TEST_TABLE_PREFIX=${TRIGGER_NAME//[-:;.,?\/]/_}_${BRANCH_NAME//[-:;.,?\/]/_}
+BUILD_DIR="/opt/odbc-driver"
+# This is the name of DSN set in odbc.ini
+export ODBC_TESTS_DSN="SampleDSNGoogleDriver"
+export LSAN_OPTIONS="use_tls=0:suppressions=/opt/odbc-driver/lsan.supp:print_suppressions=0"
 
-io::run cmake "${cmake_args[@]}" \
+export CPP_BIGQUERY_ODBC_TEST_TABLE_PREFIX=${TRIGGER_NAME//[-:;.,?]/_}_${BRANCH_NAME//[-:;.,?]/_}
+
+# Check if unixODBC is installed
+if command -v odbcinst &>/dev/null; then
+  # unixODBC is installed, export environment variable
+  export UNIXODBC_INSTALLED=true
+  echo "unixODBC is installed."
+else
+  # unixODBC is not installed
+  export UNIXODBC_INSTALLED=false
+  export ODBCINSTINI=/opt/odbc-driver/odbcinst.ini
+  export ODBCINI=/opt/odbc-driver/odbc.ini
+  echo "unixODBC is not installed."
+fi
+
+io::run cmake -B "$BUILD_DIR" \
+  "${cmake_args[@]}" \
   -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" \
   -DCMAKE_CXX_STANDARD=17 \
   -DODBC_INTEGRATION_TESTING=ON \
@@ -49,7 +83,9 @@ io::run cmake "${cmake_args[@]}" \
   -DODBC_EXAMPLES=ON \
   -DODBC_UNIT_TESTING=OFF \
   -DCLIENT_LIBRARY_INTEGRATION_TESTING=OFF
+
 io::run cmake --build cmake-out
 
+cp ci/etc/roots.pem "cmake-out/google/cloud/odbc/roots.pem"
 mapfile -t ctest_args < <(ctest::common_args)
 io::run env -C cmake-out ctest "${ctest_args[@]}"
