8f0086d0d6fa56fbcce36381e18b80b8cc8d780c
Neeraj Dwivedi <neeraj.dwivedi@37848692.corp-partner.google.com>
2026-05-08 12:09:07 +0530

    tests

--- ci/cloudbuild/builds/asan.sh
+++ ci/cloudbuild/builds/asan.sh
@@ -27,8 +27,7 @@ source module ci/cloudbuild/builds/lib/unit-tests.sh
 
 export CC=clang
 export CXX=clang++
-rm -rf ~/.cache/bazel
-rm -rf ~/.cache/bazelisk
+bazel clean --expunge
 mapfile -t args < <(bazel::common_args)
 mapfile -t unit_tests_args < <(unit_tests::bazel_args)
 args+=("--config=asan")
