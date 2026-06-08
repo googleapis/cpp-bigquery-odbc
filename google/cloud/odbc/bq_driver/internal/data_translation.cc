afdda391a42aa461e98158473717dc7d8356cf7e
Khushi Kathuria <khushi.kathuria@37848692.corp-partner.google.com>
2026-06-08 11:19:11 +0530

    test

--- google/cloud/odbc/bq_driver/internal/data_translation.cc
+++ google/cloud/odbc/bq_driver/internal/data_translation.cc
@@ -1118,7 +1118,7 @@ odbc_internal::StatusRecord ConvertFromTimestampDSValue(
         if (res_len) {
           *res_len = kTimestampBinaryLength;
         }
-        timestamp_src_struct.fraction = timestamp_src_struct.fraction * 1000;
+        timestamp_src_struct.fraction = timestamp_src_struct.fraction;
         std::memcpy(dest_buf, &timestamp_src_struct, kTimestampBinaryLength);
 
       } else {
