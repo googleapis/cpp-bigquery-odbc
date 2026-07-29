f04444f190373d9b3894847f194fc08d14c255e9
Anshu6250 <anshu@37848692.corp-partner.google.com>
2026-07-29 10:35:06 +0530

    fix: perf benchmark issues

--- google/cloud/odbc/integration_tests/odbc_driver_tests/examples/catalog_performance_example.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/examples/catalog_performance_example.cc
@@ -22,6 +22,9 @@
 #include <string>
 #include <tuple>
 #include <vector>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 namespace google::cloud::odbc_tests {
 
@@ -454,5 +457,10 @@ INSTANTIATE_TEST_SUITE_P(
 
 int main(int argc, char* argv[]) {
   testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  int result = RUN_ALL_TESTS();
+
+#ifdef _WIN32
+  TerminateProcess(GetCurrentProcess(), result);
+#endif
+  std::_Exit(result);
 }
