cc22a4c2a09444eff67f40845c0255c577c58701
Neeraj Dwivedi <neeraj.dwivedi@37848692.corp-partner.google.com>
2026-08-04 13:48:20 +0530

    new

--- google/cloud/odbc/internal/commons.h
+++ google/cloud/odbc/internal/commons.h
@@ -15,7 +15,7 @@
 #ifndef CPP_BIGQUERY_ODBC_GOOGLE_CLOUD_ODBC_INTERNAL_COMMONS_H
 #define CPP_BIGQUERY_ODBC_GOOGLE_CLOUD_ODBC_INTERNAL_COMMONS_H
 
-#include "google/cloud/odbc/bq_driver/internal/trace_utils.h"
+#include "../bq_driver/internal/trace_utils.h"
 
 namespace google::cloud::odbc_internal {
 using ::google::cloud::odbc_bq_driver_internal::LogLevel;
